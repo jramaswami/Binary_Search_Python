9352fd15d815d765b4a5a626914bb87e5e596534
Joseph Ramaswami <jramaswami@gmail.com>
2020-12-01 10:45:36 -0500

    TLE

--- regex_matching.py
+++ regex_matching.py
@@ -2,40 +2,44 @@
 binarysearch.com :: Regular Expression Matching
 https://binarysearch.com/problems/Regular-Expression-Matching
 """
+
+def solve0(pptr, sptr, pattern, string):
+    """Recursive solution."""
+    print('solve0', pptr, sptr, pattern, string)
+    if pptr >= len(pattern) and sptr >= len(string):
+        return True
+
+    if pptr + 1 < len(pattern) and pattern[pptr+1] == '*':
+        print('advance to *')
+        return solve0(pptr+1, sptr, pattern, string)
+
+    if pptr < len(pattern) and sptr < len(string) and pattern[pptr] == string[sptr]:
+        return solve0(pptr+1, sptr+1, pattern, string)
+    
+    if pptr < len(pattern) and pattern[pptr] == '.':
+        return solve0(pptr+1, sptr+1, pattern, string)
+
+    if pptr < len(pattern) and pattern[pptr] == '*':
+        if pattern[pptr-1] == '.':
+            # Ignore or consume
+            return solve0(pptr+1, sptr, pattern, string) or solve0(pptr, sptr+1, pattern, string)
+        else:
+            if sptr >= len(string):
+                return solve0(pptr+1, sptr, pattern, string)
+
+            if pattern[pptr-1] == string[sptr]:
+                # Ignore or consume
+                return solve0(pptr+1, sptr, pattern, string) or solve0(pptr, sptr+1, pattern, string)
+            else:
+                # Must ignore
+                return solve0(pptr+1, sptr, pattern, string)
+
+    return False
+                
+
 class Solution:
     def solve(self, pattern, s):
-        pattern_ptr = 0
-        string_ptr = 0
-        # TODO: Backtracking is necessary
-        while pattern_ptr < len(pattern):
-            if pattern[pattern_ptr] == '.'  and pattern_ptr + 1 < len(pattern) and pattern[pattern_ptr+1] == '*':
-                # This will match to the end of the string.
-                return True
-            if pattern_ptr + 1 < len(pattern) and pattern[pattern_ptr+1] == '*':
-                # If you found one, advance the string pointer until there are
-                # no more of the char.
-                while string_ptr < len(s) and s[string_ptr] == pattern[pattern_ptr]:
-                    string_ptr += 1
-                # Now advance the pattern_ptr past the '*'
-                pattern_ptr += 2
-            elif pattern[pattern_ptr] == '.':
-                # The '.' matches anything, advance both pointers.
-                if string_ptr >= len(s):
-                    return False
-                pattern_ptr += 1
-                string_ptr += 1
-            else:
-                # This is a regular character and must match.
-                if string_ptr >= len(s):
-                    return False
-                if pattern[pattern_ptr] != s[string_ptr]:
-                    return False
-                #If there is a match, advance both pointers.
-                pattern_ptr += 1
-                string_ptr += 1
-        # At the end, you must have matched all characters from pattern
-        # and string, so both points must be at the len.
-        return pattern_ptr == len(pattern) and string_ptr == len(s)
+        return solve0(0, 0, pattern, s)
 
 
 def test_1():
@@ -73,3 +77,16 @@ def test_6():
     pattern = "a*a"
     s = "a"
     assert solver.solve(pattern, s) == True
+
+def test_7():
+    solver = Solution()
+    pattern = "a*c"
+    s = "a"
+    assert solver.solve(pattern, s) == False
+
+def test_8():
+    solver = Solution()
+    pattern = "b*a"
+    s = "a"
+    assert solver.solve(pattern, s) == True
+
