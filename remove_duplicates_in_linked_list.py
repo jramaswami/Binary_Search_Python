5b48c11c307c33d0509ade034d0b740427a7767e
Joseph Ramaswami <jramaswami@gmail.com>
2020-12-28 15:37:42 -0500

    TLE

--- remove_duplicates_in_linked_list.py
+++ remove_duplicates_in_linked_list.py
@@ -1,6 +1,10 @@
 """
 binarysearch.com :: Remove Duplicates in Linked List
 """
+from operator import itemgetter
+from itertools import groupby
+
+
 class LLNode:
     def __init__(self, val, next=None):
         self.val = val
@@ -30,17 +34,20 @@ def ll_to_list(head):
 
 class Solution:
     def solve(self, node):
-        curr_node = node
-        while curr_node != None:
-            value = curr_node.val
-            node0 = curr_node
-            while node0.next != None:
-                if node0.next.val == value:
-                    node0.next = node0.next.next
-                else:
-                    node0 = node0.next
-            curr_node = curr_node.next
-        return node
+        if node == None:
+            return None
+        # Convert to list of tuples (value, index).
+        arr = sorted((a, i) for i, a in enumerate(ll_to_list(node)))
+        # Sort.  This will group values together with lowest index first.
+        arr.sort()
+        arr0 = [arr[0]]
+        # Compress arr into arr0 by filtering duplicates.
+        for i in range(1, len(arr)):
+            if arr[i][0] != arr[i-1][0]:
+                arr0.append(arr[i])
+        # Sort again, this time by the index.
+        arr0.sort(key=itemgetter(1))
+        return list_to_ll([a for a, _ in arr0])
 
 
 def test_1():
