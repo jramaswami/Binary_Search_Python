678fa9992a448fe284b77d13e52eb747fb8f4aac
Joseph Ramaswami <jramaswami@gmail.com>
2020-12-28 15:42:47 -0500

    AC

--- remove_duplicates_in_linked_list.py
+++ remove_duplicates_in_linked_list.py
@@ -1,10 +1,23 @@
 """
 binarysearch.com :: Remove Duplicates in Linked List
 """
-from operator import itemgetter
-from itertools import groupby
+class Solution:
+    def solve(self, node):
+        if node == None:
+            return None
+        visited = set()
+        visited.add(node.val)
+        curr_node = node
+        while curr_node.next:
+            if curr_node.next.val in visited:
+                curr_node.next = curr_node.next.next
+            else:
+                visited.add(curr_node.next.val)
+                curr_node = curr_node.next
+        return node
 
 
+# TESTING
 class LLNode:
     def __init__(self, val, next=None):
         self.val = val
@@ -32,33 +45,17 @@ def ll_to_list(head):
     return arr
 
 
-class Solution:
-    def solve(self, node):
-        if node == None:
-            return None
-        # Convert to list of tuples (value, index).
-        arr = sorted((a, i) for i, a in enumerate(ll_to_list(node)))
-        # Sort.  This will group values together with lowest index first.
-        arr.sort()
-        arr0 = [arr[0]]
-        # Compress arr into arr0 by filtering duplicates.
-        for i in range(1, len(arr)):
-            if arr[i][0] != arr[i-1][0]:
-                arr0.append(arr[i])
-        # Sort again, this time by the index.
-        arr0.sort(key=itemgetter(1))
-        return list_to_ll([a for a, _ in arr0])
-
-
 def test_1():
     ll = list_to_ll([1, 2, 1, 3])
     result = Solution().solve(ll)
     assert ll_to_list(result) == [1, 2, 3]
 
+
 def test_2():
     ll = list_to_ll([1, 1, 2, 2])
     result = Solution().solve(ll)
     assert ll_to_list(result) == [1, 2]
+
 
 def test_3():
     ll = list_to_ll([])
