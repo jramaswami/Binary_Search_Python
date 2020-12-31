320aa9dc2be72ad5bc22cd657c2562cb6458abb8
Joseph Ramaswami <jramaswami@gmail.com>
2020-12-31 15:31:37 -0500

    WA

--- update_strictly_increasing.py
+++ update_strictly_increasing.py
@@ -3,62 +3,73 @@ binarysearch.com :: Update List to Make It Strictly Increasing
 jramaswami
 """
 from math import inf
+from bisect import bisect_left, bisect_right
+from collections import defaultdict, deque
+
+
+# From Python docs.
+def index(a, x):
+    'Locate the leftmost value exactly equal to x'
+    i = bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        return i
+    return None
+
+
+def find_lt(a, x):
+    'Find rightmost value less than x'
+    i = bisect_left(a, x)
+    if i:
+        return a[i-1]
+    return None
+
+
+def find_gt(a, x):
+    'Find leftmost value greater than x'
+    i = bisect_right(a, x)
+    if i != len(a):
+        return a[i]
+    return None
 
 
 class Solution:
     def solve(self, A, B):
-        B.sort()
-        dp = [[inf for _ in A] for _ in range(len(B) + 1)]
+        B = sorted(set(B))
+        queue = deque([(0, A[0], -inf, 0)])
+        soln = inf
+        while queue:
+            curr_index, curr_value, prev_value, ops = queue.popleft()
+            if curr_index + 1 >= len(A):
+                soln = min(soln, ops)
+            else:
+                next_value = A[curr_index + 1]
+                if curr_value >= next_value:
+                    curr_value0 = find_lt(B, next_value)
+                    next_value0 = find_gt(B, curr_value)
+                    # (1) We can change curr_value to be less than next_value, if we have enough
+                    #     operations left and the new curr_value is greater than the previous max value
+                    if ops + 1 <= len(B):
+                        if curr_value0 is not None and curr_value0 > prev_value:
+                            queue.append((curr_index + 1, next_value, curr_value0, ops + 1))
+                    # (2) We can change the next_value to be greater than the curr value, if we
+                    #     have enough operations left.
+                    if ops + 1 <= len(B):
+                        if next_value0 is not None:
+                            queue.append((curr_index + 1, next_value0, curr_value, ops + 1))
+                    # (3) It is possible we need to swap the values if we have enough operations.
+                    if ops + 2 <= len(B):
+                        # If the values aren't the same we can swap them.
+                        if curr_value != next_value and index(B, curr_value) is not None and index(B, next_value) is not None:
+                            if next_value > prev_value:
+                                queue.append((curr_index + 1, next_value0, curr_value0, ops + 2))
+                else:
+                    # We do not need to change anything.
+                    queue.append((curr_index + 1, next_value, curr_value, ops))
 
-        # The invariant we will keep is that A[i-1] < A[i].
-
-        # You can always replace A[0] with B[i]
-        for i in range(len(B)):
-            dp[i][0] = 1
-        # You can leave A[0] the same.
-        dp[-1][0] = 0
-
-        for i, a in enumerate(A):
-            # We already did the column.
-            if i == 0:
-                continue
-
-            min_b_ops = inf
-            for j, b in enumerate(B):
-                # To keep our invariant, we can replace A[i] with B[j] 
-                # if A[i-1] < B[j].
-
-                # If we are going to replace A[i] with B[j]:
-                # Looking back, if we replaced A[i-1] with some B, we can
-                # replace A[i] with B[j] if A[i-1] is a B that is less then j.
-                # We sorted B, so this is true for all B[k] where k < j if
-                # B[k] < B[j].
-                if B[j-1] < B[j]:
-                    # Carry forward the minimum number of ops to replace
-                    # A[i] with B[j].  This will be the minimum ops 
-                    # for dp[0:j][i] + 1 op for the change to B[j].
-                    dp[j][i] = min(dp[j][i], min_b_ops + 1)
-                min_b_ops = min(min_b_ops, dp[j][i-1])
-
-                # We can replace A[i] with B[j] if we left A[i-1] unchanged and
-                # A[i-1] < B[j].
-                if A[i-1] < B[j]:
-                    dp[j][i] = min(dp[j][i], dp[len(B)][i-1] + 1)
-
-                # If we are going to leave A[i] unchanged.
-                # If A[i] is less than B[j] then we could have replaced
-                # A[i-1] with B[j].  So we should carry forward that number
-                # of ops + 0 to leaving A[i] unchanged, if A[i] is > B[j]
-                if A[i] > B[j]:
-                    dp[len(B)][i] = min(dp[len(B)][i], dp[j][i-1])
-
-            # Finally if we left A[i-1] unchanged, we should carry that
-            # forward if A[i] > A[i-1]
-            if A[i] > A[i-1]:
-                dp[len(B)][i] = min(dp[len(B)][i], dp[len(B)][i-1])
-
-        soln = min(row[-1] for row in dp)
-        return (soln if soln != inf else -1)
+        if soln == inf:
+            return -1
+        else:
+            return soln
 
 
 def test_1():
@@ -85,3 +96,14 @@ def test_5():
     a = [2, 1, 0]
     b = [1, 0]
     assert Solution().solve(a, b) == -1
+
+def test_6():
+    a = [0, 2, 2]
+    b = [0]
+    assert Solution().solve(a, b) == -1
+
+def test_7():
+    a = [1, 2, 2]
+    b = [1, 0]
+    assert Solution().solve(a, b) == 2
+
