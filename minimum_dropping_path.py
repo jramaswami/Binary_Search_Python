ca3cafb9eba860b00a0f5dbea03a37b4225ae41d
Joseph Ramaswami <jramaswami@gmail.com>
2021-01-05 14:26:00 -0500

    AC

--- minimum_dropping_path.py
+++ minimum_dropping_path.py
@@ -5,19 +5,42 @@ jramaswami
 from math import inf
 
 
+class MinValues:
+    """Representation of the two minimum values in a row."""
+    def __init__(self):
+        self.min1_val = inf
+        self.min1_index = -1
+        self.min2_val = inf
+        self.min2_index = -1
+
+    def add(self, index, value):
+        """Add value."""
+        if value < self.min1_val:
+            self.min2_val, self.min2_index = self.min1_val, self.min1_index
+            self.min1_val, self.min1_index = value, index
+        elif value < self.min2_val:
+            self.min2_val, self.min2_index = value, index
+
+    def get(self, index):
+        """Get minimum value, excluding current index."""
+        if index == self.min1_index:
+            return self.min2_val
+        else:
+            return self.min1_val
+
+
 class Solution:
     def solve(self, matrix):
-        dp = [[inf for _ in row] for row in matrix]
-        dp[0] = list(matrix[0])
+        curr_mins, prev_mins = MinValues(), MinValues()
         for r, row in enumerate(matrix):
             if r == 0:
-                continue
-            for c0, _ in enumerate(row):
-                for c1, _ in enumerate(row):
-                    if c0 != c1:
-                        dp[r][c0] = min(dp[r][c0], matrix[r][c0] + dp[r-1][c1])
-        return min(dp[-1])
-
+                for c, val in enumerate(row):
+                    prev_mins.add(c, val)
+            else:
+                for c, val in enumerate(row):
+                    curr_mins.add(c, val + prev_mins.get(c))
+                curr_mins, prev_mins = MinValues(), curr_mins
+        return prev_mins.min1_val
 
 
 def test_1():
