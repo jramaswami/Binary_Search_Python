2ce65a17a1f7f686ce239fa592e75fcb1f6d5268
Joseph Ramaswami <jramaswami@gmail.com>
2021-02-01 17:22:02 -0500

    WA

--- longest_bitonic_subsequence.py
+++ longest_bitonic_subsequence.py
@@ -4,39 +4,38 @@ jramaswami
 """
 class Solution:
     def solve(self, nums):
-        queue = []
-        new_queue = []
-        soln = 1
-        for num in nums:
-            # You can start here increasing
-            t0 = (num, )
-            new_queue.append((t0, +1))
-            # You can start here decreasing
-            new_queue.append((t0, -1))
+        longest_increasing = [1 for _ in nums]
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    longest_increasing[j] = longest_increasing[i] + 1
 
-            for prev_t, prev_dirn in queue:
-                # I can just to nothing
-                new_queue.append((prev_t, prev_dirn))
+        longest_decreasing = [1 for _ in nums]
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i - 1, -1, -1):
+                if nums[j] > nums[i]:
+                    longest_decreasing[j] = longest_decreasing[i]  + 1
 
-                # Can I continue an increasing sequence?
-                if prev_dirn == +1 and num > prev_t[-1]:
-                    t0 = list(prev_t)
-                    t0.append(num)
-                    new_queue.append((tuple(t0), +1))
+        max_incr = [0 for _ in longest_increasing]
+        max_incr[0] = longest_increasing[0]
+        for i in range(1, len(max_incr)):
+            max_incr[i] = max(max_incr[-1], longest_increasing[i])
 
-                # Can I change an increasing sequence to a decreasing sequence?
-                if prev_dirn == +1 and num < prev_t[-1]:
-                    t0 = list(prev_t)
-                    t0.append(num)
-                    new_queue.append((tuple(t0), -1))
-                
-                # Can I continue an decreasing sequence?
-                if prev_dirn == -1 and num < prev_t[-1]:
-                    t0 = list(prev_t)
-                    t0.append(num)
-                    new_queue.append((tuple(t0), -1))
-            queue, new_queue = new_queue, []
-        return max((len(t) for t, _ in queue))
+        max_decr = [0 for _ in longest_decreasing]
+        max_decr[-1] = longest_decreasing[-1]
+        for i in range(len(longest_decreasing) - 2, -1, -1):
+            max_decr[i] = max(max_decr[i+1], longest_decreasing[i])
+        
+        soln = 0
+        for i in range(len(nums)):
+            # The longest sequence that will increase to i and then 
+            # decrease from i
+            soln = max(soln, longest_increasing[i] + longest_decreasing[i] - 1)
+            # The longest sequence that will increase to i and then 
+            # decrease from some index greater than i
+            if i + 1 < len(nums):
+                soln = max(soln, max_incr[i] + max_decr[i+1])
+        return soln
 
 
 def test_1():
@@ -55,4 +54,6 @@ def test_4():
     nums = [795, 441, 916, 874, 990, 929, 25, 59, 391, 914, 219, 685, 208, 20, 983, 706, 831, 426, 350, 281, 77, 659, 949, 1019, 360, 271, 777, 453]
     assert Solution().solve(nums) == 10
 
-
+def test_5():
+    nums = [713, 464, 628, 504, 111, 503, 465, 148]
+    assert Solution().solve(nums) == 6
