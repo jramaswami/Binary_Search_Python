06cecf708af1182e312dace596e30d3a9ca827c3
Joseph Ramaswami <jramaswami@gmail.com>
2021-02-09 13:08:09 -0500

    AC

--- longest_strictly_increasing_then_descreasing_sublist.py
+++ longest_strictly_increasing_then_descreasing_sublist.py
@@ -10,15 +10,19 @@ class Solution:
                 longest_increasing[i+1] = longest_increasing[i] + 1
 
         longest_decreasing = [1 for _ in nums]
-        for negi, a in enumerate(nums[1:], start=-(len(nums) - 1)):
+        for negi, a in enumerate(reversed(nums[1:]), start=-(len(nums) - 1)):
             i = abs(negi)
             b = nums[i-1]
             if a < b:
                 longest_decreasing[i-1] = longest_decreasing[i] + 1
 
+        print(nums)
+        print(longest_increasing)
+        print(longest_decreasing)
+
         soln = 0
         for i, (n, incr) in enumerate(zip(nums[:-1], longest_increasing[:-1])):
-            if n > nums[i+1]:
+            if incr > 1 and n > nums[i+1]:
                 print(i, n, nums[i+1], incr, longest_decreasing[i+1], incr + longest_decreasing[i+1])
                 soln = max(incr + longest_decreasing[i+1], soln)
         return soln
@@ -39,3 +43,7 @@ def test_3():
 def test_4():
     nums = [1, 0]
     assert Solution().solve(nums) == 0
+
+def test_5():
+    nums = [2, 3, 1, 0]
+    assert Solution().solve(nums) == 4
