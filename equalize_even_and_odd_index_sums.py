5672881c7cc3d54035aceec19b217ec2ae4c2cb8
Joseph Ramaswami <jramaswami@gmail.com>
2021-02-24 15:20:45 -0500

    WA

---
+++ equalize_even_and_odd_index_sums.py
@@ -0,0 +1,42 @@
+"""
+binarysearch.com :: Equalize Even and Odd Index Sums
+jramaswami
+"""
+class Solution:
+    def solve(self, nums):
+        odd_suffix = [0 for _ in nums]
+        even_suffix = [0 for _ in nums]
+        odd_suffix.append(0)
+        even_suffix.append(0)
+
+        odd_sum = 0
+        even_sum = 0
+        for negi, n in enumerate(reversed(nums), start=-(len(nums) - 1)):
+            if n % 2:
+                odd_sum += n
+            else:
+                even_sum += n
+            odd_suffix[-negi] = odd_sum
+            even_suffix[-negi] = even_sum
+
+        soln = 0
+        odd_sum = 0
+        even_sum = 0
+        for i, n in enumerate(nums):
+            if odd_sum + even_suffix[i+1] == even_sum + odd_suffix[i+1]:
+                soln += 1
+            if i % 2:
+                odd_sum += n
+            else:
+                even_sum += n
+        return soln
+
+
+def test_1():
+    assert Solution().solve([1, 6, 4, 5]) == 1
+
+def test_2():
+    assert Solution().solve([1, 1, 0]) == 1
+
+def test_3():
+    assert Solution().solve([2, 2, 2]) == 3
