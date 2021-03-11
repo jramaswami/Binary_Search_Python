040cde01c57cc0e59d83ce6723b159ae84bdaaec
Joseph Ramaswami <jramaswami@gmail.com>
2021-03-11 07:54:26 -0500

    AC. O(N) solution.

--- square_of_a_list.py
+++ square_of_a_list.py
@@ -4,4 +4,25 @@ jramaswami
 """
 class Solution:
     def solve(self, nums):
-        return sorted(x * x for x in nums)
+        left = 0
+        right = len(nums) - 1
+        soln = []
+        while len(soln) < len(nums):
+            left_sq = nums[left] * nums[left]
+            right_sq = nums[right] * nums[right]
+            if right_sq > left_sq:
+                soln.append(right_sq)
+                right -= 1
+            else:
+                soln.append(left_sq)
+                left += 1
+        return soln[::-1]
+
+
+def test_1():
+    nums = [-9, -2, 0, 2, 3]
+    assert Solution().solve(nums) == [0, 4, 4, 9, 81]
+
+def test_2():
+    nums = [1, 2, 3, 4, 5]
+    assert Solution().solve(nums) == [1, 4, 9, 16, 25]
