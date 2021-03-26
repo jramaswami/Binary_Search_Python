e1935be926ec4e515886f373950e95e72dceaa75
Joseph Ramaswami <jramaswami@gmail.com>
2021-03-26 15:49:31 -0400

    AC. Solution that does not use builtin split function.

--- reverse_words.py
+++ reverse_words.py
@@ -4,4 +4,27 @@ jramaswami
 """
 class Solution:
     def solve(self, sentence):
-        return " ".join(reversed(sentence.split()))
+        soln = []
+        # Stack will hold a word, in reverse.
+        stack = []
+        for c in reversed(sentence):
+            # A space is found, take the word on the stack, reverse it,
+            # and put it into the solution.
+            if c == ' ':
+                while stack:
+                    soln.append(stack.pop())
+                soln.append(' ')
+            else:
+                stack.append(c)
+        # Last word on stack.
+        while stack:
+            soln.append(stack.pop())
+
+        return "".join(soln)
+
+
+
+def test_1():
+    sentence = "hello there my friend"
+    expected = "friend my there hello"
+    assert Solution().solve(sentence) == expected
