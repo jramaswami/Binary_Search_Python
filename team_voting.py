3b2e1277ba768b2d44520c4e3328d63733f015cd
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-10 11:42:57 -0400

    RTE

---
+++ team_voting.py
@@ -0,0 +1,31 @@
+"""
+binarysearch.com :: Team Voting
+jramaswami
+"""
+from collections import defaultdict
+
+
+class Solution:
+    def solve(self, votes):
+        vote_totals = defaultdict(lambda: deque([0 for _ in votes[0]]))
+        for vote in votes:
+            for i, v in enumerate(vote):
+                vote_totals[v][i] -= 1
+
+        vote_totals0 = [(v, k) for k, v in vote_totals.items()]
+        soln = []
+        for _ in enumerate(votes):
+            vote_totals0.sort(reverse=True)
+            _, c = vote_totals0.pop()
+            soln.append(c)
+        return "".join(soln)
+
+
+
+def test_1():
+    votes = ["cba", "cab", "abc"]
+    assert Solution().solve(votes) == "cab"
+
+def test_2():
+    votes = ["ba", "ab"]
+    assert Solution().solve(votes) == "ab"
