97dc63ac9414be2f65686a41fa3bc634b3e63832
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-10 11:51:50 -0400

    AC

--- team_voting.py
+++ team_voting.py
@@ -7,14 +7,15 @@ from collections import defaultdict
 
 class Solution:
     def solve(self, votes):
-        vote_totals = defaultdict(lambda: deque([0 for _ in votes[0]]))
+        vote_totals = defaultdict(lambda: [0 for _ in votes[0]])
         for vote in votes:
             for i, v in enumerate(vote):
                 vote_totals[v][i] -= 1
 
         vote_totals0 = [(v, k) for k, v in vote_totals.items()]
+        print(vote_totals0)
         soln = []
-        for _ in enumerate(votes):
+        for _ in enumerate(votes[0]):
             vote_totals0.sort(reverse=True)
             _, c = vote_totals0.pop()
             soln.append(c)
@@ -29,3 +30,7 @@ def test_1():
 def test_2():
     votes = ["ba", "ab"]
     assert Solution().solve(votes) == "ab"
+
+def test_3():
+    votes = ["cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc", "cba", "cab", "abc"]
+    assert Solution().solve(votes) == "cab"
