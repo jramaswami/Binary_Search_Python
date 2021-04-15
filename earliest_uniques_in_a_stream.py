340ede8338b81569554c09383ceea3e01730516b
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-15 11:09:23 -0400

    RTE

---
+++ earliest_uniques_in_a_stream.py
@@ -0,0 +1,40 @@
+"""
+binarysearch.com :: Earliest Uniques in a Stream
+jramaswami
+"""
+from collections import deque, Counter
+
+
+class EarliestUnique:
+    def __init__(self, nums):
+        self.nums = deque(nums)
+        self.freqs = Counter(nums)
+
+    def add(self, num):
+        self.nums.append(num)
+        self.freqs[num] += 1
+
+    def earliestUnique(self):
+        while self.freqs[self.nums[0]] > 1:
+            self.nums.popleft()
+        return self.nums[0]
+
+
+def test_1():
+    methods = ["constructor", "add", "earliestUnique", "add", "earliestUnique"]
+    arguments = [[[1, 2, 3]], [1], [], [2], []]
+    expected = [None, None, 2, None, 3]
+    eu = EarliestUnique(*arguments[0])
+    for meth, args, exp in zip(methods[1:], arguments[1:], expected[1:]):
+        res = getattr(eu, meth)(*args)
+        assert res == exp
+
+def test_2():
+    """RTE"""
+    methods = ["constructor","add","earliestUnique","add","earliestUnique"]
+    arguments = [[[1,2,3]],[1],[],[2],[]]
+    expected = [None, 3, None, -1]
+    eu = EarliestUnique(*arguments[0])
+    for meth, args, exp in zip(methods[1:], arguments[1:], expected[1:]):
+        res = getattr(eu, meth)(*args)
+        assert res == exp
