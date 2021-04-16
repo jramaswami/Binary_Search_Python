ff49b365b8fd65ccb07deabc283637fa93b53011
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-16 08:38:02 -0400

    WA

--- pair_matches_larger_than_target.py
+++ pair_matches_larger_than_target.py
@@ -6,16 +6,29 @@ class Solution:
     def solve(self, nums, target):
         nums0 = sorted(nums)
         right = 1
-        soln = 0
+        soln1 = 0
         for left, b in enumerate(nums0):
             if b is not None:
                 for right, a in enumerate(nums0):
                     if left != right and a is not None and b - a >= target:
-                        soln += 1
+                        soln1 += 1
                         nums0[right] = None
                         nums0[left] = None
                         break
-        return soln
+
+        nums0 = sorted(nums, reverse=True)
+        right = 1
+        soln2 = 0
+        for left, b in enumerate(nums0):
+            if b is not None:
+                for right, a in enumerate(nums0):
+                    if left != right and a is not None and abs(b - a) >= target:
+                        soln2 += 1
+                        nums0[right] = None
+                        nums0[left] = None
+                        break
+
+        return max(soln1, soln2)
 
 
 
@@ -37,3 +50,10 @@ def test_3():
     nums = [0, 1, 2, 2]
     target = 1
     assert Solution().solve(nums, target) == 2
+
+
+def test_4():
+    """WA"""
+    nums = [1, 1, 2, 2, 3, 3]
+    target = 1
+    assert Solution().solve(nums, target) == 3
