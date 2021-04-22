da4c3d9cf2e07c3289a06751984217b7d9655315
Joseph Ramaswami <jramaswami@gmail.com>
2021-04-22 11:53:29 -0400

    AC

--- stuck_keyboard.py
+++ stuck_keyboard.py
@@ -4,13 +4,18 @@ jramaswami
 """
 class Solution:
     def solve(self, typed, target):
-        stack = []
-        for c in typed:
-            if stack and stack[-1] == c:
-                pass
+        typed_p = 0
+        target_p = 0
+        while typed_p < len(typed):
+            if target_p < len(target) and typed[typed_p] == target[target_p]:
+                typed_p += 1
+                target_p += 1
+            elif typed_p > 0 and typed[typed_p] == typed[typed_p - 1]:
+                typed_p += 1
             else:
-                stack.append(c)
-        return "".join(stack) == target
+                return False
+        return target_p >= len(target)
+
 
 
 def test_1():
@@ -27,3 +32,18 @@ def test_3():
     typed = "bb"
     target = "bb"
     assert Solution().solve(typed, target) == True
+
+def test_4():
+    typed = "aaabbbbbbbcccccddddd"
+    target = "abcd"
+    assert Solution().solve(typed, target) == True
+
+def test_5():
+    typed = "aaabbbbbbbcccccddddd"
+    target = "abbcccddddd"
+    assert Solution().solve(typed, target) == True
+
+def test_6():
+    typed = "aaabbbbbbbcccccddd"
+    target = "abbcccdddddd"
+    assert Solution().solve(typed, target) == False
