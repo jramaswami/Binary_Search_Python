0fa0556fcef59daccbb623fd2071d74637582ed8
Joseph Ramaswami <jramaswami@gmail.com>
2021-05-07 16:09:50 -0400

    AC

---
+++ wolves_of_wallstreet.py
@@ -0,0 +1,44 @@
+"""
+binarysearch.com :: Wolves of Wall Street
+jramaswami
+"""
+class Solution:
+    def solve(self, prices):
+        i = 0
+        soln = 0
+        while i < len(prices):
+            # Find valley
+            while i + 1 < len(prices) and prices[i] > prices[i + 1]:
+                i += 1
+            if i < len(prices):
+                buy = prices[i]
+                i += 1
+
+            # Find peak
+            while i + 1 < len(prices) and prices[i] < prices[i + 1]:
+                i += 1
+            if i < len(prices):
+                sell = prices[i]
+                soln += sell - buy
+                i += 1
+        return soln
+
+
+def test_1():
+    prices = [1, 5, 3, 4, 6]
+    assert Solution().solve(prices) == 7
+
+
+def test_2():
+    prices = [5, 4, 3, 2, 1]
+    assert Solution().solve(prices) == 0
+
+
+def test_3():
+    prices = [4, 1, 5, 1, 4, 2, 5, 8, 1, 8, 3, 1, 6, 6, 2, 4, 4, 5, 2, 7]
+    assert Solution().solve(prices) == 33
+
+
+def test_4():
+    prices = [1, 5, 4, 10, 1, 3, 3, 1, 5, 5, 2, 3, 10, 2, 2, 5, 5, 4, 1, 5]
+    assert Solution().solve(prices) == 31
