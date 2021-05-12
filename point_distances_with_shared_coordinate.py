52e8ce63530d6c891e4523bdd302c238056ce375
Joseph Ramaswami <jramaswami@gmail.com>
2021-05-12 16:38:33 -0400

    AC

--- point_distances_with_shared_coordinate.py
+++ point_distances_with_shared_coordinate.py
@@ -2,6 +2,7 @@
 binarysearch.com :: Point Distances with Shared Coordinate
 jramaswami
 """
+import bisect
 from collections import defaultdict
 from math import inf
 
@@ -10,27 +11,26 @@ class Solution:
     def solve(self, points):
         points_by_x = defaultdict(list)
         points_by_y = defaultdict(list)
-        for x, y in points:
-            points_by_x[x].append(y)
-            points_by_y[y].append(x)
+        for i, (x, y) in enumerate(points):
+            bisect.insort(points_by_x[x], (y, i))
+            bisect.insort(points_by_y[y], (x, i))
 
-        soln = []
-        for x, y in points:
-            d = inf
-            first_dupe = True
-            for y0 in points_by_x[x]:
-                if y0 == y and first_dupe:
-                    first_dupe = False
-                    continue
-                d = min(d, abs(y - y0))
-            first_dupe = True
-            for x0 in points_by_y[y]:
-                if x0 == x and first_dupe:
-                    first_dupe = False
-                    continue
-                d = min(d, abs(x - x0))
-            soln.append(d)
+        soln = [inf for _ in points]
+        for x in points_by_x:
+            for (left, i), (right, j) in zip(points_by_x[x][:-1], points_by_x[x][1:]):
+                dist = abs(left - right)
+                soln[i] = min(soln[i], dist)
+                soln[j] = min(soln[j], dist)
+
+        for y in points_by_y:
+            for (left, i), (right, j) in zip(points_by_y[y][:-1], points_by_y[y][1:]):
+                dist = abs(left - right)
+                soln[i] = min(soln[i], dist)
+                soln[j] = min(soln[j], dist)
+
         return soln
+
+        
             
 
 def test_1():
