0955ee45540ef2be5529b5daddbb0a687b2ef828
Joseph Ramaswami <jramaswami@gmail.com>
2021-05-28 16:56:56 -0400

    AC. More elegant solution.

--- sorted_elements.py
+++ sorted_elements.py
@@ -4,24 +4,9 @@ jramaswami
 """
 
 
-from collections import defaultdict
-
-
 class Solution:
     def solve(self, nums):
-        # Find the proper position for each element.
-        # Be sure to handle the case of duplicates in nums.
-        nums0 = sorted(nums)
-        positions = defaultdict(set)
-        for i, n in enumerate(nums0):
-            positions[n].add(i)
-
-        soln = 0
-        for i, n in enumerate(nums):
-            if i in positions[n]:
-                soln += 1
-
-        return soln
+        return sum(a == b for a, b in zip(nums, sorted(nums)))
 
 
 
