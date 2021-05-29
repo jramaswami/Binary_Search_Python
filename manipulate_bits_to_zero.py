400706ef435e3380ec2f3d24162938e58e71d0ca
Joseph Ramaswami <jramaswami@gmail.com>
2021-05-29 12:14:32 -0400

    Still TLE on test 4, even with shortcut at a power of 2.

--- manipulate_bits_to_zero.py
+++ manipulate_bits_to_zero.py
@@ -5,6 +5,7 @@ jramaswami
 
 
 from collections import deque
+from math import inf
 
 
 def op1(n):
@@ -41,30 +42,38 @@ def op2(n):
 
 class Solution:
     def solve(self, num):
+        pow2 = set([1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,
+                    16384,32768,65536,131072,262144,524288,1048576,
+                    2097152,4194304,8388608,16777216,33554432,
+                    67108864,134217728,268435456,536870912,1073741824,
+                    2147483648,4294967296,8589934592])
         visited = set()
         queue = deque()
         queue.append((num, 0))
         visited.add(num)
+        soln = inf
         while queue:
-            n, ops = queue[0]
-            n = op1(n)
-            if n == 0:
-                return ops + 1
+            n0, ops = queue.popleft()
+            if ops > soln:
+                pass
+            elif n0 == 0:
+                soln = min(soln, ops + 1)
+            elif n0 in pow2:
+                soln = min(soln, ops + ((2 * n0) - 1))
+            else:
+                n = n0
+                n = op1(n)
+                if n not in visited:
+                    queue.append((n, ops + 1))
+                    visited.add(n)
 
-            if n not in visited:
-                queue.append((n, ops + 1))
-                visited.add(n)
+                n = n0
+                n = op2(n)
+                if n not in visited:
+                    queue.append((n, ops + 1))
+                    visited.add(n)
 
-            n, ops = queue[0]
-            n = op2(n)
-            if n == 0:
-                return ops + 1
-
-            if n not in visited:
-                queue.append((n, ops + 1))
-                visited.add(n)
-            # Remove n from queue
-            queue.popleft()
+        return soln
 
 
 def test_1():
@@ -79,7 +88,7 @@ def test_3():
     assert Solution().solve(174781) == 209705
 
 
-# def test_4():
-#     """TLE"""
-#     n = 2147483647
-#     assert Solution().solve(n) == 209705
+def test_4():
+    """TLE"""
+    n = 2147483647
+    assert Solution().solve(n) == 209705
