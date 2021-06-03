e250d249391a9b6059631411b81f4ca7aa7a561e
Joseph Ramaswami <jramaswami@gmail.com>
2021-06-03 14:51:29 -0400

    TLE

---
+++ largest_square_submatrix.py
@@ -0,0 +1,155 @@
+"""
+binarysearch.com :: Largest Square Submatrix
+jramaswami
+"""
+
+
+from itertools import accumulate
+
+
+class SubmatrixSums:
+    def __init__(self, matrix):
+        self.sums = [list(accumulate(row)) for row in matrix]
+        for r, row in enumerate(self.sums):
+            for c, _ in enumerate(row):
+                if r - 1 >= 0:
+                    self.sums[r][c] += self.sums[r-1][c]
+
+    def query(self, row, col):
+        """Return sum of submatrix from 0, 0 to row, col (inclusive)."""
+        if row < 0 or col < 0:
+            return 0
+        return self.sums[row][col]
+
+    def max_square(self, row, col):
+        """Return the max square with top left corner at (row, col)"""
+        delta = 0
+        soln = 0
+        while row + delta < len(self.sums) and col + delta < len(self.sums[0]):
+            # Find the submatrix sum from (row, col) to (row0, col0) exclusive
+            row0 = row + delta
+            col0 = col + delta
+            sqrt = delta + 1
+
+            A = self.query(row0, col0) 
+            B = self.query(row0, col - 1) 
+            C = self.query(row - 1, col0) 
+            D = self.query(row - 1, col - 1)
+            s = (A - B - C + D)
+
+            if s == sqrt * sqrt:
+                soln = max(soln, sqrt * sqrt)
+            delta += 1
+        return soln
+
+    def __str__(self):
+        return "\n".join(str(row) for row in self.sums)
+
+
+class Solution:
+    def solve(self, matrix):
+        submatrix_sums = SubmatrixSums(matrix)
+        soln = 0
+        for r, row in enumerate(matrix):
+            for c, _ in enumerate(row):
+                soln = max(soln, submatrix_sums.max_square(r, c))
+        return soln
+
+
+def test_1():
+    matrix = [
+        [0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0]
+    ]
+    assert Solution().solve(matrix) == 16
+
+
+def test_2():
+    matrix = [
+        [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 1, 0, 1, 1, 1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0]
+    ]
+    assert Solution().solve(matrix) == 9
+
+def test_3():
+    matrix = [[0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
+              [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+              [0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+              [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1],
+              [1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+              [1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+              [0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
+              [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+              [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0],
+              [1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+              [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+              [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+              [1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0]]
+    assert Solution().solve(matrix) == 9
+
+def test_4():
+    matrix = [[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1], 
+              [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1], 
+              [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1], 
+              [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1], 
+              [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1], 
+              [1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+              [1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0]]
+    assert Solution().solve(matrix) == 25
+
+
+import random
+from timeit import default_timer as timer
+
+
+def generate_random_matrix(size):
+    """Genrate a random matrix."""
+    return [[random.choice([0,1,1,1,1,1,1]) for _ in range(size)] for _ in range(size)]
+
+
+def main():
+    """For timing against a large matrix."""
+    matrix = generate_random_matrix(200)
+    start = timer()
+    Solution().solve(matrix)
+    stop = timer()
+    print(f"{stop - start} seconds.")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
