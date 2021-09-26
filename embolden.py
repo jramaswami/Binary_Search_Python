e345b93b3c2624dd50ccaf30fd6f7b25e8bdcc5a
Joseph Ramaswami <jramaswami@gmail.com>
2021-09-26 16:15:12 -0400

    WA

---
+++ embolden.py
@@ -0,0 +1,52 @@
+"""
+binarysearch.com :: Embolden
+jramaswami
+"""
+
+class Solution:
+
+    def solve(self, text, patterns):
+        bold = [0 for _ in text]
+        for pattern in patterns:
+            index = text.find(pattern)
+            while index >= 0:
+                for i in range(index, index + len(pattern)):
+                    bold[i] = 1
+                index = text.find(pattern, index + len(pattern))
+
+        soln = []
+        for i, c in enumerate(text):
+            if bold[i] and i == 0:
+                soln.append('<b>')
+            elif bold[i] and not bold[i-1]:
+                soln.append('<b>')
+
+            soln.append(c)
+
+            if bold[i] and i == len(text) - 1:
+                soln.append('</b>')
+            elif bold[i] and not bold[i+1]:
+                soln.append('</b>')
+
+        return "".join(soln)
+
+
+def test_1():
+    text = "abcdefg"
+    patterns = ["bc", "ef"]
+    expected = "a<b>bc</b>d<b>ef</b>g"
+    assert Solution().solve(text, patterns) == expected
+
+
+def test_2():
+    text = "abcdefg"
+    patterns = ["bc", "de"]
+    expected = "a<b>bcde</b>fg"
+    assert Solution().solve(text, patterns) == expected
+
+
+def test_3():
+    text = "cbbabcacbbccbacbaccbbcabcabbbccbcbbcaaabcacbaabaabaccacaaccbbbaaaabcbccaacbcacabbcbacbbcbbccccbababc"
+    patterns = ["bba", "bbb", "baa", "baa", "aba", "bab", "abb", "abb", "bbb", "aab"]
+    expected = "c<b>bbab</b>cacbbccbacbaccbbcabc<b>abbb</b>ccbcbbca<b>aab</b>cac<b>baabaaba</b>ccacaacc<b>bbbaaaab</b>cbccaacbcac<b>abb</b>cbacbbcbbcccc<b>babab</b>c"
+    assert Solution().solve(text, patterns) == expected
