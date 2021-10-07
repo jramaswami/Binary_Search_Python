77fef4e44fe3582de7f8b241eac68d03a694bee6
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-07 16:25:03 -0400

    WA

--- binary_sublist_with_target_sum.py
+++ binary_sublist_with_target_sum.py
@@ -4,38 +4,56 @@ jramaswami
 """
 
 
+from collections import deque
+
+
 class Solution:
 
-    def solve(self, nums, target):
-        # Determine the min index of 1 to the right of any zero.
-        one_right = [-1 for _ in nums]
-        curr_one = -1
-        for i, _ in enumerate(nums):
-            j = len(nums) - i - 1
-            if nums[j] == 1:
-                curr_one = j
-            one_right[j] = curr_one
-
+    def solve(self, nums, target_sum):
         soln = 0
-        left = 0
-        right = -1
+        window = deque()
         curr_sum = 0
-        while right < len(nums):
-            # Add one to the right.
-            right += 1
-            if right >= len(nums):
-                break
-            curr_sum += nums[right]
+        for n in nums:
+            if n == 1 and curr_sum == target_sum:
+                # Adding the next element will take us over the target sum.
+                if target_sum == 0:
+                    # If the target sum is zero then any sublist of current
+                    # window will sum to 0.
+                    soln += ((len(window) * (len(window) + 1)) // 2)
+                    # Since the target sum is zero, just empty the window
+                    # for the next element.
+                    window.clear()
+                    curr_sum = 0
+                else:
+                    # If the target sum is not zero, then we can just count
+                    # every window ending at current end if the curr_sum
+                    # equals the target_sum
+                    while curr_sum == target_sum:
+                        soln += 1
+                        curr_sum -= window[0]
+                        window.popleft()
+                    # The curr_sum is now one less than the target sum.
+                    # We can add the 1 to get the current sum.
+                    window.append(1)
+                    curr_sum += 1
+            else:
+                window.append(n)
+                curr_sum += n
 
-            # If sum is too much, reduce sum.
-            while curr_sum > target:
-                curr_sum -= nums[left]
-                left += 1
-
-            # The number of sublists ending at right is the number of sublists
-            # from current left to the first one to to the right of left.
-            if curr_sum == target:
-                soln += (1 + one_right[left] - left)
+        # There still might be items in the window.
+        if window and target_sum == curr_sum:
+            if target_sum == 0:
+                # If the target sum is zero then any sublist of current
+                # window will sum to 0.
+                soln += ((len(window) * (len(window) + 1)) // 2)
+            else:
+                # If the target sum is not zero, then we can just count
+                # every window ending at current end if the curr_sum
+                # equals the target_sum
+                while curr_sum == target_sum:
+                    soln += 1
+                    curr_sum -= window[0]
+                    window.popleft()
 
         return soln
 
@@ -52,4 +70,33 @@ def test_2():
     nums = [1]
     target = 0
     expected = 0
+    assert Solution().solve(nums, target) == expected
+
+
+def test_3():
+    nums = [0, 0, 0, 1, 0]
+    target = 0
+    expected = 7
+    assert Solution().solve(nums, target) == expected
+
+
+def test_4():
+    nums = [0, 0, 0, 0, 0]
+    target = 0
+    expected = 15
+    assert Solution().solve(nums, target) == expected
+
+
+def test_5():
+    nums = []
+    target = 0
+    expected = 0
+    assert Solution().solve(nums, target) == expected
+
+
+def test_6():
+    """WA"""
+    nums = [0, 0, 0, 1, 0]
+    target = 0
+    expected = 1
     assert Solution().solve(nums, target) == expected
