14ef143c49db5eb01856e878fd32e5c327099b82
Joseph Ramaswami <jramaswami@gmail.com>
2021-10-13 17:26:56 -0400

    AC

--- largest_rectangle_submatrix.py
+++ largest_rectangle_submatrix.py
@@ -31,13 +31,21 @@ class Solution:
 
         # Left to right
         for r, row in enumerate(height):
+            # Stack holds the leftmost index for a given height.
+            # Stack invariant: heights are in ascending order.
             stack = []
             for c, ht in enumerate(row):
                 # Add current index to stack.
                 stack.append(Item(ht, c))
 
-                # Fix the stack by "cutting off the tops."
-                stack = [Item(min(ht, s.height), s.index) for s in stack]
+                # Fix the stack.  Find the leftmost item with a height greater
+                # than ht.  Replace that item with an item at the same index
+                # but with a height of ht.
+                leftmost_index = c
+                while stack and stack[-1].height >= ht:
+                    leftmost_index = min(c, stack[-1].index)
+                    stack.pop()
+                stack.append(Item(ht, leftmost_index))
 
                 soln = max(soln,
                         max(
@@ -69,4 +77,10 @@ def test_3():
 def test_4():
     matrix = [ [1, 1, 1, 1], [1, 0, 0, 1], [1, 1, 1, 1], [1, 1, 1, 1] ]
     expected = 8
+    assert Solution().solve(matrix) == expected
+
+
+def test_5():
+    matrix = [[1 for _ in range(250)] for _ in range(250)]
+    expected = 250 * 250
     assert Solution().solve(matrix) == expected
