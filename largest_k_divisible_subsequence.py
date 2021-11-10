15d40db2dba6c679dd79469c0b24d94c9d0ec61c
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-10 13:55:43 -0500

    RTE

--- largest_k_divisible_subsequence.py
+++ largest_k_divisible_subsequence.py
@@ -4,28 +4,20 @@ jramaswami
 """
 
 
-import functools
-
-
 class Solution:
     def solve(self, nums, k):
+        # dp will hold the maximum sum % k for each index.
+        dp = [[0 for _ in range(k)] for _ in nums]
+        dp[0][nums[0] % k] = nums[0]
 
-        @functools.cache
-        def solve0(i, acc):
-            # Base case.
-            if i >= len(nums):
-                if acc % k:
-                    return 0
-                else:
-                    return acc
-
-            # With the value at nums[i].
-            max_with = solve0(i + 1, acc + nums[i])
-            # Without the value at nums[i]
-            max_without = solve0(i + 1, acc)
-            return max(max_with, max_without)
-
-        return solve0(0, 0)
+        for i, n in enumerate(nums[1:], start=1):
+            for remainder, max_sum in enumerate(dp[i-1]):
+                # For each maximum sum per remainder, add n and then
+                # store if it is the maximum for the new remainder.
+                new_sum = max_sum + n
+                new_remainder = new_sum % k
+                dp[i][new_remainder] = max(dp[i][new_remainder], new_sum)
+        return dp[-1][0]
 
 
 def test_1():
@@ -47,4 +39,12 @@ def test_3():
     nums = [1] * 200
     k = 10
     expected = 200
+    assert Solution().solve(nums, k) == expected
+
+
+def test_4():
+    """RTE"""
+    nums = []
+    k = 4
+    expected = 0
     assert Solution().solve(nums, k) == expected
