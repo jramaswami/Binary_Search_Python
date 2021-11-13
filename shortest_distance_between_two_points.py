f269c51def56753e21bdf85674b24fb08c25e29c
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-13 16:19:56 -0500

    AC

--- shortest_distance_between_two_points.py
+++ shortest_distance_between_two_points.py
@@ -1,74 +1,90 @@
 """
 binarysearch.com :: Shortest Distance Between Two Points
 jramaswami
+
+REF: https://medium.com/@andriylazorenko/closest-pair-of-points-in-python-79e2409fc0b2
 """
 
 
-import collections
+import itertools
 import math
+
+
+def brute(ax):
+    """Brute force comparison of each pair of points to find closest pair."""
+    best_d = math.inf
+    best_pair = None
+    for pt1, pt2 in itertools.combinations(ax, 2):
+        d2 = math.pow(pt1[0] - pt2[0], 2) + math.pow(pt1[1] - pt2[1], 2)
+        if d2 < best_d:
+            best_d, best_pair = d2, (pt1, pt2)
+    return best_pair, best_d
+
+
+def closest_pair(ax, ay):
+    """Recursive function to find closest pair."""
+
+    # If the lenght of ax is small enough, use brute force.
+    len_ax = len(ax)
+    if len_ax <= 3:
+        return brute(ax)
+
+    # Split array by x coordinates in two, that is, split the plane in two
+    # at ax[mid][x].
+    mid = len_ax // 2
+    left_x = ax[:mid]
+    right_x = ax[mid:]
+
+    # Partition array by y coordinates on their x coordinate such that
+    # left_y contains points with pt[x] <= ax[mid][x] and right_y
+    # contains points with pt[x] <= ax[mid][x], splitting the plane in two.
+    midpoint = ax[mid][0]
+    left_y = []
+    right_y = []
+    for pt in ay:
+        if pt[0] <= midpoint:
+            left_y.append(pt)
+        else:
+            right_y.append(pt)
+
+    # Call recursively to find the closest pair in each side of the division.
+    best_pair1, best_d1 = closest_pair(left_x, left_y)
+    best_pair2, best_d2 = closest_pair(right_x, right_y)
+    if best_d1 <= best_d2:
+        best_d = best_d1
+        best_pair = best_pair1
+    else:
+        best_d = best_d2
+        best_pair = best_pair2
+
+    # Now check for points that span the division.
+    midpoint_x = ax[mid][0]
+    # Choose coordinates from ay that are within best_d by x coordinate.
+    box = [pt for pt in ay if abs(midpoint_x - pt[0]) <= best_d]
+    # Brute force a comparison to see if there are any points closer then
+    # our previous closest pair.
+    box_best_pair, box_best_d = brute(box)
+    if box_best_d < best_d:
+        best_pair, best_d = box_best_pair, box_best_d
+
+    return best_pair, best_d
 
 
 class Solution:
 
     def solve(self, coordinates):
-
-        def distance_squared(x1, y1, x2, y2):
-            dx = x1 - x2
-            dy = y1 - y2
-            return (dx * dx) + (dy * dy)
-
-
-        # Determine range of x and y coordinates.
-        min_x, max_x = min_y, max_y = -math.inf, math.inf
+        # Guard against stacking the points in a single x.
+        min_x, max_x = min_y, max_y = math.inf, -math.inf
         for x, y in coordinates:
             min_x, max_x = min(min_x, x), max(max_x, x)
             min_y, max_y = min(min_y, y), max(max_y, y)
+        if max_y - min_y > max_x - min_x:
+            coordinates = [(y, x) for x, y in coordinates]
 
-        # Use the axis with the greatest range of numbers as the axis of
-        # the line sweep.
-        dx = max_x - min_x
-        dy = max_y - min_y
-        if dy > dx:
-            X = 1
-            Y = 0
-        else:
-            X = 0
-            Y = 1
-
-        best_distance_squared = math.inf
-        # Sort coordinates left to right by x.
-        coordinates.sort(key=lambda p: p[X])
-        # We define our box as coordinates[left:right].
-        left = 0
-        for right, point in enumerate(coordinates[1:], start=1):
-            x1, y1 = point[X], point[Y]
-            print(f"({x1}, {y1}) box={coordinates[left:right]} {best_distance_squared=}")
-
-            # Remove from box all items that are too far away on the x-axis.
-            # Binary search for the index of the left-most coordinate within
-            # the distance squared by x value.
-            lo = left
-            hi = right
-            new_left = right
-            while lo <= hi:
-                mid = lo + ((hi - lo) // 2)
-                x2 = coordinates[mid][X]
-                dx = x1 - x2
-                if (dx * dx) <= best_distance_squared:
-                    new_left = min(new_left, mid)
-                    hi = mid - 1
-                else:
-                    lo = mid + 1
-            left = new_left
-
-            print(f"({x1}, {y1}) box={coordinates[left:right]} {best_distance_squared=} {left=}")
-            for j in range(left, right):
-                x2, y2 = coordinates[j][X], coordinates[j][Y]
-                d2 = distance_squared(x1, y1, x2, y2)
-                if d2 < best_distance_squared:
-                    best_distance_squared = d2
-
-        return best_distance_squared
+        ax = sorted(coordinates, key=lambda pt: pt[0])
+        ay = sorted(coordinates, key=lambda pt: pt[1])
+        pr, d2 = closest_pair(ax, ay)
+        return d2
 
 
 def test_1():
@@ -120,4 +136,16 @@ def test_5():
         [2, 0]
     ]
     expected = 4
+    assert Solution().solve(coordinates) == expected
+
+
+def test_6():
+    """WA"""
+    coordinates = [
+        [0, 2],
+        [2, 0],
+        [2, 3],
+        [3, 0]
+    ]
+    expected = 1
     assert Solution().solve(coordinates) == expected
