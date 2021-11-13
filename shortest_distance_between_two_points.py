333488753411d49d9fdc3ab14b0a27bddba2aeac
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-13 12:58:40 -0500

    WA

--- shortest_distance_between_two_points.py
+++ shortest_distance_between_two_points.py
@@ -20,24 +20,30 @@ class Solution:
         best_distance_squared = math.inf
         # Sort coordinates left to right by x.
         coordinates.sort()
-        box = collections.deque([coordinates[0]])
-        for x1, y1 in coordinates[1:]:
+        # We define our box as coordinates[left:right].
+        left = 0
+        for right, (x1, y1) in enumerate(coordinates[1:], start=1):
 
             # Remove from box all items that are too far away on the x-axis.
-            while box:
-                x2 = box[0][0]
+            # Binary search for the index of the left-most coordinate within
+            # the distance squared by x value.
+            lo = left
+            hi = right
+            while lo < hi:
+                mid = lo + ((hi - lo) // 2)
+                x2 = coordinates[mid][0]
                 dx = x1 - x2
                 if (dx * dx) <= best_distance_squared:
-                    break
-                box.popleft()
+                    left = mid
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
 
-            for x2, y2 in box:
+            for j in range(left, right):
+                x2, y2 = coordinates[j]
                 d2 = distance_squared(x1, y1, x2, y2)
                 if d2 < best_distance_squared:
                     best_distance_squared = d2
-
-            # Add current point to box.
-            box.append((x1, y1))
 
         return best_distance_squared
 
@@ -73,4 +79,22 @@ def test_3():
         [3, 0],
     ]
     expected = 5
+    assert Solution().solve(coordinates) == expected
+
+
+def test_4():
+    N = 100000
+    coordinates = [[0, y] for y in range(0, N)]
+    expected = 1
+    assert Solution().solve(coordinates) == expected
+
+
+def test_5():
+    """WA"""
+    coordinates = [
+        [0, 0],
+        [1, 3],
+        [2, 0]
+    ]
+    expected = 4
     assert Solution().solve(coordinates) == expected
