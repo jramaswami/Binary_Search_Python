77937c0994fef01a0ea08ba2abb14553f0548798
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-17 16:07:30 -0500

    AC

--- line_segment.py
+++ line_segment.py
@@ -15,17 +15,23 @@ class Solution:
         # where m = (y1 - y2) / (x1 - x2)
         x1, y1 = coordinates[0]
         x2, y2 = coordinates[1]
-        m = (y1 - y2) / (x1 - x2)
 
-        # Use point slope form to determine the linear equation.
-        # y - y1 = m(x - x1)
-        # y = m(x - x1) + y1
-        # Then iterate over all coordinates to see if they satisfy
-        # the equation.
-        for x, y in coordinates:
-            if y != (m * (x - x1)) + y1:
-                return False
-        return True
+        if x1 == x2:
+            # This will yield an infinite slope or vertical line.
+            # A vertical line requires all x values to be the same.
+            return all(x == x1 for x, _ in coordinates)
+        else:
+            m = (y1 - y2) / (x1 - x2)
+
+            # Use point slope form to determine the linear equation.
+            # y - y1 = m(x - x1)
+            # y = m(x - x1) + y1
+            # Then iterate over all coordinates to see if they satisfy
+            # the equation.
+            for x, y in coordinates:
+                if y != (m * (x - x1)) + y1:
+                    return False
+            return True
 
 
 def test_1():
@@ -52,8 +58,9 @@ def test_3():
     """RTE"""
     coordinates = [
         [1, 1],
-        [3, 3],
-        [7, 7]
+        [1, 2],
+        [3, 4],
+        [3, 5]
     ]
     expected = False
     assert Solution().solve(coordinates) == expected
