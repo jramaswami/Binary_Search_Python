1c64c7886aa31e9075f86a8228af413100bf9c37
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-18 13:03:01 -0500

    AC

--- unique_subsequences_equal_to_target.py
+++ unique_subsequences_equal_to_target.py
@@ -4,11 +4,6 @@ jramaswami
 """
 
 
-import collections
-import sys
-
-
-sys.setrecursionlimit(pow(10, 9))
 MOD = pow(10, 9) + 7
 
 
@@ -19,26 +14,26 @@ class Solution:
         if T == "":
             return 0
 
-        letter_indexes = collections.defaultdict(list)
+        # DP: columns are S, rows are T.
+        dp = [[0 for _ in S] for _ in T]
+
+        # Initialize first row as 1 for every T[0] in S.
         for i, c in enumerate(S):
-            letter_indexes[c].append(i)
+            if c == T[0]:
+                dp[0][i] = 1
 
-        def traverse(s_index, t_index):
-            # Base case: we have reached the end of T.  This is one way
-            # to do so.
-            if t_index >= len(T):
-                return 1
+        # Now DP.
+        for r, curr_c in enumerate(T[1:], start=1):
+            running = 0
+            for c, val in enumerate(dp[r]):
+                if S[c] == curr_c:
+                    dp[r][c] = running
+                running = (running + dp[r-1][c]) % MOD
 
-            result = 0
-            c = T[t_index]
-            # Go to every possible c in S such thsat the index of c is greater
-            # than the current s_index.
-            for s_index0 in letter_indexes[c]:
-                if s_index0 > s_index:
-                    result = (result + traverse(s_index0, t_index + 1)) % MOD
-            return result
-
-        return traverse(-1, 0) % MOD
+        soln = 0
+        for k in dp[-1]:
+            soln = (soln + k) % MOD
+        return soln
 
 
 def test_1():
@@ -53,4 +48,12 @@ def test_2():
     S = "hello"
     T = ""
     expected = 0
+    assert Solution().solve(S, T) == expected
+
+
+def test_3():
+    S = "itmsanjvabhkadrhgbqujzukrbmausyxaxpucjhbakicbfcgtyrhzeziyidavzombofrbcjfqsqobgcioghtchzkotusyrisczulhlgfwpkbhceyittmaxvfskpgkbhhcofoxnyyomnotrcyjpogdlqajlrspztofiwibarllpidqgsokqtuksleohttjqsqnoqfrpyguyhtekhsovdvtlmwggdingehlhnubgsjfjzacrsobhybvnmtlgzsfrraddaylvsggmykhqdawuzpikgaadrovzdrisugqmqdvhwlbficvrbekq"
+
+    T = "ijbuyfftzgheaibalsfhvrydaadl"
+    expected = 2633472
     assert Solution().solve(S, T) == expected
