1bf8fecaea7ea8ae0364dc82488f8cab885f73d1
Joseph Ramaswami <jramaswami@gmail.com>
2021-11-18 12:47:40 -0500

    WA

---
+++ unique_subsequences_equal_to_target.py
@@ -0,0 +1,54 @@
+"""
+binarysearch.com :: Unique Subsequences Equal to Target
+jramaswami
+"""
+
+
+import collections
+import sys
+
+
+sys.setrecursionlimit(pow(10, 9))
+MOD = pow(10, 9) + 7
+
+
+class Solution:
+
+    def solve(self, S, T):
+        letter_indexes = collections.defaultdict(list)
+        for i, c in enumerate(S):
+            letter_indexes[c].append(i)
+
+        def traverse(s_index, t_index):
+            # Base case: we have reached the end of T.  This is one way
+            # to do so.
+            if t_index >= len(T):
+                return 1
+
+            result = 0
+            c = T[t_index]
+            # Go to every possible c in S such thsat the index of c is greater
+            # than the current s_index.
+            for s_index0 in letter_indexes[c]:
+                if s_index0 > s_index:
+                    result = (result + traverse(s_index0, t_index + 1)) % MOD
+            return result
+
+        return traverse(-1, 0) % MOD
+
+
+def test_1():
+    S = "ello"
+    T = "el"
+    expected = 2
+    assert Solution().solve(S, T) == expected
+
+
+def test_2():
+    S = "a" * 1000
+    T = "a" * 1000
+    expected = 1
+    assert Solution().solve(S, T) == expected
+
+
+
