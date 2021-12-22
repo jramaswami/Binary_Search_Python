5cacd615afd5f329fd11a11da1f0939ef6124f5d
Joseph Ramaswami <jramaswami@gmail.com>
2021-12-22 18:43:46 -0500

    WA

--- triangle_triplets.py
+++ triangle_triplets.py
@@ -3,6 +3,15 @@ binarysearch.com :: Triangle Triplets
 jramaswami
 """
 
+from bisect import bisect_right
+
+
+def count_lte(a, x):
+    i = bisect_right(a, x)
+    if i:
+        return i-1
+    return -1
+
 
 class Solution:
 
@@ -11,12 +20,16 @@ class Solution:
         nums.sort()
         for i, a in enumerate(nums):
             for j, b in enumerate(nums[i+1:], start=i+1):
-                for k, c in enumerate(nums[j+1:], start=j+1):
-                    if a + b > c:
-                        soln += 1
+                soln += 1 + count_lte(nums[j+1:], a + b)
         return soln
 
 
 def test_1():
     nums = [7, 8, 8, 9, 100]
     assert Solution().solve(nums) == 4
+
+
+def test_2():
+    "WA"
+    nums = [1, 1, 0]
+    assert Solution().solve(nums) == 0
