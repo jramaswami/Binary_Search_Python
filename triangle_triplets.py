1b69c3512902936bb40e9c287773cde40e83dfba
Joseph Ramaswami <jramaswami@gmail.com>
2021-12-25 13:59:21 -0500

    AC

--- triangle_triplets.py
+++ triangle_triplets.py
@@ -1,27 +1,32 @@
 """
 binarysearch.com :: Triangle Triplets
 jramaswami
+
+REF: https://www.geeksforgeeks.org/find-number-of-triangles-possible/
 """
-
-from bisect import bisect_right
-
-
-def count_lte(a, x):
-    i = bisect_right(a, x)
-    if i:
-        return i-1
-    return -1
 
 
 class Solution:
 
     def solve(self, nums):
-        soln = 0
+        # You cannot have side of length 0.
         nums = sorted(n for n in nums if n > 0)
-        for i, a in enumerate(nums):
+        soln = 0
+        # Fix the side a at index i.
+        for i, a in enumerate(nums[:-2]):
+            # Initialize side c at index k = i + 2.
+            k = i + 2
+            # Move side b from indexes i+1 to the end of the array.
             for j, b in enumerate(nums[i+1:], start=i+1):
-                soln += 1 + count_lte(nums[j+1:], a + b)
+                # Find the right most element larger than a + b.
+                while k < len(nums) and a + b > nums[k]:
+                    k += 1
+                # If k is more than j then the number of sides that can be c
+                # are nums[j+1:k]
+                if k > j:
+                    soln += (k - j - 1)
         return soln
+
 
 
 def test_1():
