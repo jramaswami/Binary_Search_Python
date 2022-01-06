c43960c3544ad840dc8e60b9e507cf6841766972
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-06 08:30:37 -0500

    TLE

---
+++ nearest_bus_stop_from_a_house.py
@@ -0,0 +1,80 @@
+"""
+binarysearch.com :: Nearest Bus Stop From a House
+jramaswami
+"""
+
+
+import enum
+import collections
+
+
+QItem = collections.namedtuple('QItem', ['row', 'col', 'dist'])
+
+
+class CellType(enum.IntEnum):
+    EMPTY = 0
+    WALL = 1
+    HOUSE = 2
+    BUS_STOP = 3
+
+
+class Solution:
+
+    def solve(self, matrix):
+
+        OFFSETS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+        def inbounds(r, c):
+            "Return True if matrix[r][c] is inside matrix."
+            return r >= 0 and c >= 0 and r < len(matrix) and c < len(matrix[r])
+
+        def neighbors(r, c):
+            "Return the neighbors of matrix[r][c]"
+            for dr, dc in OFFSETS:
+                r0, c0 = r + dr, c + dc
+                if inbounds(r0, c0):
+                    yield r0, c0
+
+        def navigable(r, c):
+            "Return True if you can walk through matrix[r][c]."
+            if matrix[r][c] == CellType.WALL or matrix[r][c] == CellType.HOUSE:
+                return False
+            return True
+
+        # Gather bus stops.
+        queue = collections.deque()
+        for r, row in enumerate(matrix):
+            for c, val in enumerate(row):
+                if val == CellType.HOUSE:
+                    queue.append(QItem(r, c, 0))
+
+        # BFS
+        while queue:
+            item = queue.popleft()
+            if matrix[item.row][item.col] == CellType.BUS_STOP:
+                return item.dist
+            for r0, c0 in neighbors(item.row, item.col):
+                if navigable(r0, c0):
+                    queue.append(QItem(r0, c0, item.dist + 1))
+        return -1
+
+
+def test_1():
+    matrix = [
+        [2, 1, 3, 0],
+        [1, 1, 1, 1],
+        [0, 3, 0, 0],
+        [0, 0, 0, 2]
+    ]
+    assert Solution().solve(matrix) == 3
+
+
+def test_2():
+    "WA.  Endless loop because I didn't keep track of visited cells."
+    matrix = [
+        [0, 2, 0],
+        [1, 2, 0],
+        [3, 1, 0]
+    ]
+    assert Solution().solve(matrix) == -1
+
