450fa8bfa3c44f5a9e6b0e8b551cb005aacb55dc
Joseph Ramaswami <jramaswami@gmail.com>
2022-01-17 11:01:55 -0500

    AC

--- points_on_a_line.py
+++ points_on_a_line.py
@@ -11,11 +11,18 @@ import math
 class Solution:
 
     def solve(self, coordinates):
+        # Corner case: no points at all.
         if not coordinates:
             return 0
 
         soln = 1
         for i, p1 in enumerate(coordinates):
+
+            # Only check if there are enough points to make solution bigger.
+            points_left = len(coordinates) - i
+            if points_left < soln:
+                break
+
             lines = collections.defaultdict(int)
             for j, p2 in enumerate(coordinates[i+1:], start=i+1):
                 # Compute slope.
@@ -27,6 +34,7 @@ class Solution:
                 # Same slope means same line.
                 lines[m] += 1
                 soln = max(soln, 1 + lines[m])
+
         return soln
 
 
@@ -49,3 +57,9 @@ def test_2():
         [3, 0]
     ]
     assert Solution().solve(coordinates) == 1
+
+
+def test_3():
+    "TLE"
+    coordinates = [(i, i) for i in range(1000)]
+    assert Solution().solve(coordinates) == 1000
