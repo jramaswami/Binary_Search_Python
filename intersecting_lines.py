be7fac67e740324ee7adb2b6eea1c3aceb72d320
Joseph Ramaswami <jramaswami@gmail.com>
2022-02-10 11:28:05 -0500

    Have a possible solution but it requires a balanced search tree.

--- intersecting_lines.py
+++ intersecting_lines.py
@@ -38,10 +38,10 @@ def on_segment(pi, pj, pk):
 
 
 def segments_intersect(p1, p2, p3, p4):
-    d1 = direction(p3, p4, p1)
-    d2 = direction(p3, p4, p2)
-    d3 = direction(p1, p2, p3)
-    d4 = direction(p1, p2, p4)
+    d1 = direction(p3, p4, p1);
+    d2 = direction(p3, p4, p2);
+    d3 = direction(p1, p2, p3);
+    d4 = direction(p1, p2, p4);
     if ((d1 > 0 and d2 < 0) or (d1 < 0 and d2 > 0)) and ((d3 > 0 and d4 < 0) or (d3 < 0 and d4 > 0)):
         return True
     elif d1 == 0 and on_segment(p3, p4, p1):
