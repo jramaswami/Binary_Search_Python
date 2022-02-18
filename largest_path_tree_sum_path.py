4dae2a6bd0352d7dbe4a1b7d0a1e67e1a4ee96a6
Joseph Ramaswami <jramaswami@gmail.com>
2022-02-18 17:44:59 -0500

    AC

--- largest_path_tree_sum_path.py
+++ largest_path_tree_sum_path.py
@@ -4,35 +4,24 @@ jramaswami
 """
 
 
+import math
+
+
 class Solution:
     def solve(self, root):
 
+        answer = -math.inf
         def traverse(node):
             if node is None:
-                return 0, 0
+                return 0
 
-            left_max_path, left_max_sum = traverse(node.left)
-            right_max_path, right_max_sum = traverse(node.right)
+            x = dfs(node.left)
+            y = dfs(node.right)
+            answer = max(answer, x + y + node.val)
+            return max(0, node.val + max(x, y))
 
-            # Max path is the best "leg" to use.  It can be the best from the
-            # right, the best from the left, or just the current node.
-            my_max_path = node.val + max(left_max_path, right_max_path, 0)
-            # Max sum is the best path sum that is below or including the
-            # current node.  It will be either the best sum from the right,
-            # the best sum from the left, the node + left leg, the node +
-            # right leg, the node + both legs, or even just the node.
-            my_max_sum = max(
-                left_max_sum,
-                right_max_sum,
-                node.val,
-                node.val + left_max_path,
-                node.val + right_max_path,
-                node.val + left_max_path + right_max_path
-            )
-
-            return my_max_path, my_max_sum
-
-        return traverse(root)[0]
+        traverse(root)
+        return answer
 
 
 # WA: [5, [-2, null, null], [-2, null, null]]
