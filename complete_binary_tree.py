6cf138edc67f0f7407ba52ec5fca8edca9043a85
Joseph Ramaswami <jramaswami@gmail.com>
2022-02-22 17:01:06 -0500

    RTE

--- complete_binary_tree.py
+++ complete_binary_tree.py
@@ -3,14 +3,20 @@ binarysearch.com :: Complete Binary Tree
 jramaswami
 """
 
+
+import math
+
+
 class Solution:
 
     def solve(self, root):
 
+        node_count = 0
+        level_count = 0
         queue = [root]
         new_queue = []
         while queue:
-
+            level_count += 1
             # Check queue is valid.
             first_none = len(queue)
             last_value = -1
@@ -25,12 +31,17 @@ class Solution:
             # BFS
             for node in queue:
                 if node:
+                    node_count += 1
                     new_queue.append(node.left)
                     new_queue.append(node.right)
 
+
+            if all(node is None for node in new_queue):
+                break
+
             queue, new_queue = new_queue, []
 
-        return True
+        return int(math.ceil(math.log2(node_count))) == level_count
 
 
 #
@@ -61,6 +72,13 @@ def test_3():
 
 def test_4():
     "WA"
-    root = make_tree([0, [1, null, [0, null, null]], [0, null, null]])
+    root = make_tree([0, [1, [1, null, null], [0, null, null]], null])
     expected = False
+    assert Solution().solve(root) == expected
+
+
+def test_5():
+    "RTE"
+    root = None
+    expected = True
     assert Solution().solve(root) == expected
