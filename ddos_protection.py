035eefdb1613fbcf04d20321be43e862fd532e45
Joseph Ramaswami <jramaswami@gmail.com>
2022-02-26 18:44:07 -0500

    AC

--- ddos_protection.py
+++ ddos_protection.py
@@ -16,21 +16,20 @@ class Solution:
         requests0 = [Request(t, u) for u, t in requests]
         requests0.sort()
 
-        urqs = collections.defaultdict(collections.deque)
+        urqs = collections.defaultdict(int)
         grq = collections.deque()
 
         processed_requests = 0
         for request in requests0:
             # Remove expired requests.
-            for uid, urq in urqs.items():
-                while urq and urq[0].timestamp <= request.timestamp - 60:
-                    urq.popleft()
             while grq and grq[0].timestamp <= request.timestamp - 60:
+                uid = grq[0].userid
+                urqs[uid] -= 1
                 grq.popleft()
 
-            if len(urqs[request.userid]) < user_max and len(grq) < global_max:
+            if urqs[request.userid] < user_max and len(grq) < global_max:
                 grq.append(request)
-                urqs[request.userid].append(request)
+                urqs[request.userid] += 1
                 processed_requests += 1
 
         return processed_requests
