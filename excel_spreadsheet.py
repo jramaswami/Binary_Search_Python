22c11ef4e9078917169170776195f04133f1421a
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-29 16:20:45 -0400

    AC

--- excel_spreadsheet.py
+++ excel_spreadsheet.py
@@ -7,45 +7,43 @@ jramaswami
 class Solution:
     def solve(self, matrix):
         # Don't mutate original matrix.
-        matrix0 = [list(row) for row in matrix]
+        matrix0 = [[None for _ in row] for row in matrix]
 
         def label_to_indexes(cell):
             c = ord(cell[0]) - ord('A')
-            r = int(cell[1]) - 1
+            r = int(cell[1:]) - 1
             return r, c
 
-        def indexes_to_label(r, c):
-            return chr(ord('A') + c) + str(r + 1)
-
-        def eval_cell(cell):
+        def xeval(x):
+            # Is this a number?
             try:
-                v = int(cell)
+                v = int(x)
                 return v
             except:
                 pass
 
-            r, c = label_to_indexes(cell)
-            if matrix0[r][c][0] == '=':
-                # Formula
-                if '+' in matrix0[r][c]:
-                    left_ref, right_ref = matrix0[r][c][1:].split('+')
-                    left_val, right_val = eval_cell(left_ref), eval_cell(right_ref)
-                    matrix0[r][c] = str(left_val + right_val)
+            # Is this a formula?
+            if x.startswith('='):
+                if '+' in x:
+                    a, b = x[1:].split('+')
+                    u, v = xeval(a), xeval(b)
+                    return u + v
                 else:
-                    left_ref, right_ref = matrix0[r][c][1:].split('-')
-                    left_val, right_val = eval_cell(left_ref), eval_cell(right_ref)
-                    matrix0[r][c] = str(left_val - right_val)
-            else:
-                # Cell ref
-                v = eval_cell(matrix0[r][c])
-                matrix0[r][c] = str(v)
-            return int(matrix0[r][c])
+                    a, b = x[1:].split('-')
+                    u, v = xeval(a), xeval(b)
+                    return u - v
 
-        for r, row in enumerate(matrix0):
+            # This must be a cell reference.
+            r, c = label_to_indexes(x)
+            if matrix0[r][c] is None:
+                matrix0[r][c] = xeval(matrix[r][c])
+            return matrix0[r][c]
+
+        for r, row in enumerate(matrix):
             for c, _ in enumerate(row):
-                eval_cell(indexes_to_label(r, c))
+                matrix0[r][c] = xeval(matrix[r][c])
 
-        return matrix0
+        return [[str(x) for x in row] for row in matrix0]
 
 
 
