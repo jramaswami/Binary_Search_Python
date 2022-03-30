58c8674dd1dcf680661a5daf4f5f0bd6fd8822b2
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-30 18:54:05 -0400

    WA

---
+++ remove_sublist_to_reach_equilibrium.py
@@ -0,0 +1,50 @@
+"""
+binarysearch.com :: Remove Sublist to Reach Equilibrium
+jramaswami
+"""
+
+
+class Solution:
+
+    def solve(self, nums, k):
+        def sign(n):
+            if n > k:
+                return 1
+            if n < k:
+                return -1
+            return 0
+
+        nums0 = [sign(n) for n in nums]
+        delta = sum(nums0)
+        prev_sums = dict()
+        curr_sum = 0
+        soln = len(nums)
+        for i, n in enumerate(nums0):
+            curr_sum = curr_sum + n
+            look_for = curr_sum - delta
+            if look_for in prev_sums:
+                soln = min(soln, i - prev_sums[look_for])
+            prev_sums[curr_sum] = i
+        return len(nums) - soln
+
+
+def test_1():
+    nums = [5, 9, 7, 8, 2, 4]
+    k = 5
+    expected = 5
+    assert Solution().solve(nums, k) == expected
+
+
+def test_2():
+    nums = [1, 2, 3]
+    k = 4
+    expected = 0
+    assert Solution().solve(nums, k) == expected
+
+
+def test_3():
+    "WA"
+    nums = [0]
+    k = 0
+    expected = 1
+    assert Solution().solve(nums, k) == expected
