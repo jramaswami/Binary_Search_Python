cd22bf8d38d87233dbeafcb0df2efe873434c90f
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-01 14:29:05 -0400

    AC

--- parity_jump.py
+++ parity_jump.py
@@ -16,27 +16,34 @@ class Solution:
             return nums[index] % 2
 
         dist = [math.inf for _ in nums]
+        rgraph = [[] for _ in nums]
 
         # Find index, i, which can jump directly to an
         # index, j, with an opposite parity number.
         Q = collections.deque()
         for i, n in enumerate(nums):
-            if i - n >= 0 and parity(i) != parity(i - n):
-                dist[i] = 1
-                Q.append(i)
-            elif i + n < len(nums) and parity(i) != parity(i + n):
-                dist[i] = 1
-                Q.append(i)
+            if i - n >= 0:
+                if parity(i) != parity(i - n):
+                    dist[i] = 1
+                    Q.append(i)
+                else:
+                    # Reverse graph: i - n can be reached by i
+                    rgraph[i - n].append(i)
+            if i + n < len(nums):
+                if parity(i) != parity(i + n):
+                    dist[i] = 1
+                    Q.append(i)
+                else:
+                    # Reverse graph: i + n can be reached by i
+                    rgraph[i + n].append(i)
 
         # BFS to find answer.
         while Q:
             i = Q.popleft()
-            if i - n >= 0 and dist[i - n] > dist[i] + 1:
-                Q.append(i - n)
-                dist[i - n] = dist[i] + 1
-            if i + n < len(nums) and dist[i + n] > dist[i] + 1:
-                Q.append(i + n)
-                dist[i + n] = dist[i] + 1
+            for j in rgraph[i]:
+                if dist[j] > dist[i] + 1:
+                    dist[j] = dist[i] + 1
+                    Q.append(j)
 
         return [-1 if d == math.inf else d for d in dist]
 
