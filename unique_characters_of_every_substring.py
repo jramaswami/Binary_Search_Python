c78a2b0554e57e7b6fa091b944b3136c460576a1
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-04 14:01:14 -0400

    Still TLE.

--- unique_characters_of_every_substring.py
+++ unique_characters_of_every_substring.py
@@ -4,7 +4,7 @@ jramaswami
 """
 
 
-import collections
+import string
 
 
 MOD = pow(10, 9) + 7
@@ -13,14 +13,17 @@ MOD = pow(10, 9) + 7
 class Solution:
 
     def solve(self, S):
-        def count_unique_chars(l, r):
-            freqs = collections.Counter(S[l:r+1])
-            return sum(1 if v == 1 else 0 for v in freqs.values())
-
         soln = 0
-        for l, _ in enumerate(S):
-            for r, _ in enumerate(S[l:], start=l):
-                soln = (soln + count_unique_chars(l, r)) % MOD
+        dp = [[] for _ in range(len(S) + 1)]
+        dp[-1] = [0 for _ in range(26)]
+
+        for r, c in enumerate(S):
+            # Initialize curr dp.
+            dp[r] = list(dp[r-1])
+            dp[r][ord(c) - ord('a')] += 1
+            for l, _ in enumerate(S[:r+1]):
+                unique_chars = sum(dp[r][j] - dp[l-1][j] == 1 for j in range(26))
+                soln = (soln + unique_chars) % MOD
         return soln
 
 
@@ -35,4 +38,11 @@ def test_2():
     "TLE"
     s = "amhjlzxrafkttezhlxiodvcoqultfdpqhisqvzqshfbnqpawsrwktdgwfjoapdntevcqudvsafmialicdtitciufkeqlebbwiahsbtgayvgahaqwwvlefybzswrnmgugjngnthurtfhrgkdmbbkshphektiddijnqbzlvufccfcfxdqsvyednhesvtgfzrtyvhbqpnbzqbpsmjuqmhpuypicllmhacijsoelsleymntxkotllmengyvnhuvbolcbwcjzxjxhplazkzzpfgftumaqqpoqolihnooetsmuhafcjcmjvmlbftzbtkxvhggszzutaxipeeqmifgirukpwcxjacxquknysjhnfiprsoniuumecrceoivibzgtfnclthaogxvdjndreacjmwccohscfqmpkofvtbfqkkgbrgpokkqwmcctvzsdgvwbpomwkfvnxrlrfnkxfieoogjudonmmkjzhpyhqkpznycoghwltvnyxexvwrwldieycbpgmkmwimvbjbqyhbpagbbcmatewcaieskdmmjixuqmwirijrhnpjbytvmhkdvvcffczbwdjzsfxazfkcntsoonfvfhjamflilmyuqugsyqbuirrlonjzuzkllyjztvgxpplobxboaphanyaldzslqqowtfripepetzemrbiktpvbuqnwjazerdfxqdxpayvtscvkyizfdrqmvchtephzqxpqwvvisxkrdeqypwzxkuyrdvagfjovciytg"
     expected = 457278
+    assert Solution().solve(s) == expected
+
+
+def test_3():
+    "TLE"
+    s = "aufohslfhlbzekgzshgjrycraeuqakddlwzauassvrjsebafjtsyftqhxyauviwmqytjkkcbynjfdxzbuoltrapvtpjniqsqvdqvkfwvusilbointpcjqlkanbjdtnwvsgximmnqarxmglbtenusdkhxcwgpcliqfrhtogygynfbmxqqlakiozzcggefsczsspahggyipulqrcfmbnjhzulomluqmpzdybnurakwnketbgfdwojjbcstamgcxlhjsptbgjotjdmbcoodmxqejqttoumollbxurvanlsyrznqtypwimxfcxlzglhszvuswzrciqwizmaevqhuktaenwavcoxyatdakqlqkehuxyhixgypvxiufcagzujzhtbvqsssjdtsfwouccxzivduysshswnhcpceaczflprixnpusmuwuovceqgnnbuzzurerxhanmyvcudovypwhpjdrlsuknkuczjehocnyggoqljyvsvezopghhxsvcchjmtifqxvggnlqiicmkgbhvineidycgvdcztmkhewhcdqdmnmdoghumavxxesrijmzkxgwiutlunderfjogjfadlisrgvbhwgtuwyavvqricsiduthpixipltzogbwvqopocivpannlayriobeoisqnbyzcliobqvhnjwmtonmwznhecfotuanbuhotnmggbbufwehaivwlbyyvuaywnnolirlehcyjgkafolirjpdqolmslmwrtrxswoprtaqmpxyegcwwamnvohomdaihtjuphzhrbfaqzfuunybkwcxsbsnwqondglnmhkgzabivmfessnwxntixnukxalitaxrfwyljlefzrcjmprekesqmhskpauglihmglbggbjctrzsskeluwymmhebzcfdjojhvxnmvhvqoesrxnsvhnvvwmpxkn"
+    expected = 603477
     assert Solution().solve(s) == expected
