bf46ee83c11c328c38304c7232ba77a8b5ccccc9
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-04 13:52:00 -0400

    TLE. Brute force.

--- unique_characters_of_every_substring.py
+++ unique_characters_of_every_substring.py
@@ -7,73 +7,32 @@ jramaswami
 import collections
 
 
+MOD = pow(10, 9) + 7
+
+
 class Solution:
 
     def solve(self, S):
-        MOD = pow(10, 9) + 7
-
-        def count_substrings(unique_target):
-            freqs = collections.defaultdict(int)
-            window = collections.deque()
-            unique_chars = 0
-            i = 0
-            while i < len(S) and unique_chars < unique_target:
-                window.append((S[i], i))
-                freqs[S[i]] += 1
-                if freqs[S[i]] == 1:
-                    unique_chars += 1
-                elif freqs[S[i]] > 1:
-                    unique_chars -= 1
-                i += 1
-
-            # Invariant: window will always satisfy unique_target == unique_chars
-            result = 0
-            while i < len(S):
-                if freqs[S[i]] == 0:
-                    # New character.
-                    while window and unique_chars == unique_target:
-                        # print('substr', window)
-                        result = (result + 1) % MOD
-                        freqs[window[0][0]] -= 1
-                        if freqs[window[0][0]] == 0:
-                            unique_chars -= 1
-                        # print('popping', window[0])
-                        window.popleft()
-                        # print(f"{window=} {unique_chars=} {freqs=}")
-                    unique_chars += 1
-                elif freqs[S[i]] == 1:
-                    unique_chars -= 1
-                else:
-                    # print('substr', window)
-                    result = (result + 1) % MOD
-                # Add to window.
-                freqs[S[i]] += 1
-                window.append((S[i], i))
-                i += 1
-
-            # print(f"{i=} {window=} {unique_chars=} {freqs=}")
-            while window and unique_chars == unique_target:
-                # print('substr', window)
-                result = (result + 1) % MOD
-                freqs[window[0][0]] -= 1
-                if freqs[window[0][0]] == 0:
-                    unique_chars -= 1
-                window.popleft()
-
-            return result
-
+        def count_unique_chars(l, r):
+            freqs = collections.Counter(S[l:r+1])
+            return sum(1 if v == 1 else 0 for v in freqs.values())
 
         soln = 0
-        # print(S)
-        for i in range(1, 27):
-            # print('target = ', i)
-            k = count_substrings(i)
-            t = (k * i) % MOD
-            soln = (soln + t) % MOD
-        return soln % MOD
+        for l, _ in enumerate(S):
+            for r, _ in enumerate(S[l:], start=l):
+                soln = (soln + count_unique_chars(l, r)) % MOD
+        return soln
+
 
 
 def test_1():
     s = "aab"
     expected = 6
+    assert Solution().solve(s) == expected
+
+
+def test_2():
+    "TLE"
+    s = "amhjlzxrafkttezhlxiodvcoqultfdpqhisqvzqshfbnqpawsrwktdgwfjoapdntevcqudvsafmialicdtitciufkeqlebbwiahsbtgayvgahaqwwvlefybzswrnmgugjngnthurtfhrgkdmbbkshphektiddijnqbzlvufccfcfxdqsvyednhesvtgfzrtyvhbqpnbzqbpsmjuqmhpuypicllmhacijsoelsleymntxkotllmengyvnhuvbolcbwcjzxjxhplazkzzpfgftumaqqpoqolihnooetsmuhafcjcmjvmlbftzbtkxvhggszzutaxipeeqmifgirukpwcxjacxquknysjhnfiprsoniuumecrceoivibzgtfnclthaogxvdjndreacjmwccohscfqmpkofvtbfqkkgbrgpokkqwmcctvzsdgvwbpomwkfvnxrlrfnkxfieoogjudonmmkjzhpyhqkpznycoghwltvnyxexvwrwldieycbpgmkmwimvbjbqyhbpagbbcmatewcaieskdmmjixuqmwirijrhnpjbytvmhkdvvcffczbwdjzsfxazfkcntsoonfvfhjamflilmyuqugsyqbuirrlonjzuzkllyjztvgxpplobxboaphanyaldzslqqowtfripepetzemrbiktpvbuqnwjazerdfxqdxpayvtscvkyizfdrqmvchtephzqxpqwvvisxkrdeqypwzxkuyrdvagfjovciytg"
+    expected = 457278
     assert Solution().solve(s) == expected
