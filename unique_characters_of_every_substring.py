f2cba58eddd00bf3cc1b9a914542a2a4da72c205
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-04 15:03:56 -0400

    AC

--- unique_characters_of_every_substring.py
+++ unique_characters_of_every_substring.py
@@ -1,6 +1,9 @@
 """
 binarysearch.com :: Unique Characters of Every Substring
 jramaswami
+
+
+REF: https://www.youtube.com/watch?v=eN8zATT702M
 """
 
 
@@ -13,19 +16,26 @@ MOD = pow(10, 9) + 7
 class Solution:
 
     def solve(self, S):
+        # Record the indexes (1-based) for every letter.
+        # Include 0 for the left and the end of the array for the right.
+        dp = {c: [0] for c in string.ascii_lowercase}
+        for i, c in enumerate(S):
+            dp[c].append(i+1)
+        for c in string.ascii_lowercase:
+            dp[c].append(len(S)+1)
+
+        # For each index, determine the number of substrings for which it
+        # will be the only occurrence.  The is the same as the number of
+        # substrings between the previous occurrence and next ocurrence.
+        # (Or start or end of the array if there isn't a previous or next
+        # occurrence).
         soln = 0
-        dp = [[] for _ in range(len(S) + 1)]
-        dp[-1] = [0 for _ in range(26)]
-
-        for r, c in enumerate(S):
-            # Initialize curr dp.
-            dp[r] = list(dp[r-1])
-            dp[r][ord(c) - ord('a')] += 1
-            for l, _ in enumerate(S[:r+1]):
-                unique_chars = sum(dp[r][j] - dp[l-1][j] == 1 for j in range(26))
-                soln = (soln + unique_chars) % MOD
+        for c in string.ascii_lowercase:
+            if len(dp[c]) > 2:
+                for i in range(1, len(dp[c])-1):
+                    k = ((dp[c][i] - dp[c][i-1]) * (dp[c][i+1]- dp[c][i])) % MOD
+                    soln = (soln + k) % MOD
         return soln
-
 
 
 def test_1():
@@ -45,4 +55,10 @@ def test_3():
     "TLE"
     s = "aufohslfhlbzekgzshgjrycraeuqakddlwzauassvrjsebafjtsyftqhxyauviwmqytjkkcbynjfdxzbuoltrapvtpjniqsqvdqvkfwvusilbointpcjqlkanbjdtnwvsgximmnqarxmglbtenusdkhxcwgpcliqfrhtogygynfbmxqqlakiozzcggefsczsspahggyipulqrcfmbnjhzulomluqmpzdybnurakwnketbgfdwojjbcstamgcxlhjsptbgjotjdmbcoodmxqejqttoumollbxurvanlsyrznqtypwimxfcxlzglhszvuswzrciqwizmaevqhuktaenwavcoxyatdakqlqkehuxyhixgypvxiufcagzujzhtbvqsssjdtsfwouccxzivduysshswnhcpceaczflprixnpusmuwuovceqgnnbuzzurerxhanmyvcudovypwhpjdrlsuknkuczjehocnyggoqljyvsvezopghhxsvcchjmtifqxvggnlqiicmkgbhvineidycgvdcztmkhewhcdqdmnmdoghumavxxesrijmzkxgwiutlunderfjogjfadlisrgvbhwgtuwyavvqricsiduthpixipltzogbwvqopocivpannlayriobeoisqnbyzcliobqvhnjwmtonmwznhecfotuanbuhotnmggbbufwehaivwlbyyvuaywnnolirlehcyjgkafolirjpdqolmslmwrtrxswoprtaqmpxyegcwwamnvohomdaihtjuphzhrbfaqzfuunybkwcxsbsnwqondglnmhkgzabivmfessnwxntixnukxalitaxrfwyljlefzrcjmprekesqmhskpauglihmglbggbjctrzsskeluwymmhebzcfdjojhvxnmvhvqoesrxnsvhnvvwmpxkn"
     expected = 603477
+    assert Solution().solve(s) == expected
+
+
+def test_4():
+    s = "aaabbb"
+    expected = 12
     assert Solution().solve(s) == expected
