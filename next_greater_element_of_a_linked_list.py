21d46afc517dcdb0c62e09070e2312ab88852244
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-15 11:01:54 -0400

    AC. Faster iterative solution.

--- next_greater_element_of_a_linked_list.py
+++ next_greater_element_of_a_linked_list.py
@@ -7,33 +7,30 @@ jramaswami
 class Solution:
 
     def solve(self, root):
-
-        def traverse(node):
-            if node is None:
-                return []
-
-            # Stack is monotonically increasing and contains
-            stack = traverse(node.next)
-            # Make stack contain only values greater than the
-            # value of the current node.
-            while stack and stack[-1] <= node.val:
+        # Monotonically decreasing stack containing nodes to the left
+        # of current node (if any).
+        stack = []
+        curr_node = root
+        while curr_node:
+            # For any nodes less than current node, the current node
+            # is the next node to the right with a greater value.
+            # Remove them from stack and change their value to the current
+            # node's value.
+            while stack and stack[-1].val < curr_node.val:
+                stack[-1].val = curr_node.val
                 stack.pop()
+            # All nodes in the stack have a value greater than or equal
+            # to the current node.  Adding the current node to the stack
+            # maintains the monotonically decreasing invariant.
+            stack.append(curr_node)
+            # Move to the next node.
+            curr_node = curr_node.next
 
-            curr_val = node.val
-            if stack:
-                # If there is a value in the stack there is a node
-                # to the right of the current node that is greater.
-                node.val = stack[-1]
-            else:
-                # If the stack is empty there is no node greater
-                # than the current node.  Set it to 0.
-                node.val = 0
+        # Any nodes remaining on the stack did not have a node with a
+        # greater value to their right.  Change the value of those nodes
+        # to zero.
+        while stack:
+            stack[-1].val = 0
+            stack.pop()
 
-            # Add the current node's previous value to the stack.
-            # This value is less than any node currently on the stack.
-            stack.append(curr_val)
-
-            return stack
-
-        traverse(root)
         return root
