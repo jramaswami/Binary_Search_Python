2733ccd3b503a02140ccd5998d457dfd4f64d553
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-18 12:05:04 -0400

    WA

---
+++ substringify.py
@@ -0,0 +1,38 @@
+"""
+binarysearch.com :: Substringify
+jramaswami
+"""
+
+
+class Solution:
+
+    def solve(self, S, T):
+
+        def compute_changes(i):
+            result = 0
+            for s, t in zip(S[i:], T):
+                if s != t:
+                    result += 1
+            return result
+
+        soln = len(T)
+        for i, _ in enumerate(S[:len(T)]):
+            if i + len(T) > len(S):
+                break
+            soln = min(soln, compute_changes(i))
+        return soln
+
+
+def test_1():
+    s = "foobar"
+    t = "oops"
+    expected = 2
+    assert Solution().solve(s, t) == expected
+
+
+def test_2():
+    "WA"
+    s = "va"
+    t = "a"
+    expected = 0
+    assert Solution().solve(s, t) == expected
