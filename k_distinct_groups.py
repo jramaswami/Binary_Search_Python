3e29f59bceceb76ffb5526e3c87b8b07aec47be7
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-22 10:38:30 -0400

    WA

---
+++ k_distinct_groups.py
@@ -0,0 +1,48 @@
+"""
+binarysearch.com :: K-Distinct Groups
+jramaswami
+"""
+
+
+class Solution:
+    def solve(self, counts, k):
+
+        def check(groups):
+            r = groups * k
+            for x in counts:
+                r -= min(x, groups, r)
+                if r == 0:
+                    return True
+            return False
+
+        soln = 0
+        lo, hi = 0, sum(counts)
+        while lo <= hi:
+            mid = lo = ((hi - lo) // 2)
+            if check(mid):
+                soln = max(mid, soln)
+                hi -= 1
+            else:
+                lo += 1
+        return soln
+
+def test_1():
+    counts = [3, 3, 2, 5]
+    k = 2
+    expected = 6
+    assert Solution().solve(counts, k) == expected
+
+
+def test_2():
+    counts = [3, 2, 4]
+    k = 3
+    expected = 2
+    assert Solution().solve(counts, k) == expected
+
+
+def test_3():
+    "WA"
+    counts = [1, 0]
+    k = 1
+    expected = 1
+    assert Solution().solve(counts, k) == expected
