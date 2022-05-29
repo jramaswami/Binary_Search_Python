7bdc6fc32896168dbbd0658ebd51aeec5519ab8f
Joseph Ramaswami <jramaswami@gmail.com>
2022-05-29 09:21:36 -0400

    AC

--- special_nodes.py
+++ special_nodes.py
@@ -9,22 +9,32 @@ class Solution:
     def solve(self, tree, color):
 
         def traverse(node, parent):
-            "Returns (number of nodes in subtree, colors, soln accumulator)"
+            "Returns (number of nodes in subtree, colors, soln accumulator, flag)"
             if node is None:
-                return 0, set(), 0
+                return 0, set(), 0, True
 
             my_nodes = 1
             my_colors = set([color[node]])
             my_acc = 0
+            my_flag = True
             for child in tree[node]:
                 if child != parent:
-                    child_nodes, child_colors, child_acc = traverse(child, node)
+                    child_nodes, child_colors, child_acc, child_flag = traverse(child, node)
+                    my_flag = my_flag and child_flag
                     my_nodes += child_nodes
-                    my_colors |= child_colors
                     my_acc += child_acc
-            if len(my_colors) == my_nodes:
-                my_acc += 1
+                    if my_flag:
+                        if len(child_colors) > len(my_colors):
+                            child_colors.update(my_colors)
+                            my_colors = child_colors
+                        else:
+                            my_colors.update(child_colors)
 
-            return my_nodes, my_colors, my_acc
+            if my_flag:
+                if len(my_colors) == my_nodes:
+                    my_acc += 1
+            else:
+                my_colors = set()
+            return my_nodes, my_colors, my_acc, my_flag
 
-        return traverse(0, -1)
+        return traverse(0, -1)[-2]
