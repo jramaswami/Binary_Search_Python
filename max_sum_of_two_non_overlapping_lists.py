1f030e70efcf8b6eaf1cf9742f45057c55075588
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-01 10:10:00 -0400

    AC. Struggled to get the initial best sum correct.

--- max_sum_of_two_non_overlapping_lists.py
+++ max_sum_of_two_non_overlapping_lists.py
@@ -30,17 +30,34 @@ class Solution:
             curr_max = max(curr_max, sums[i])
             prefix_maxs[i] = curr_max
 
+        print(f"{a=} {b=}")
+        print(f"{sums=}")
+        print(f"{suffix_maxs=}")
+        print(f"{prefix_maxs=}")
+        print(f"{nums=}")
+
         soln = -math.inf
         curr_sum = sum(nums[:b])
-        if i + a < len(suffix_maxs):
-            soln = max(soln, curr_sum + suffix_maxs[i+a])
+        # Curr window is from [0:b)
+        # Look for the best prev window ending at or after b.
+        # This is equivalent to b + a.
+        if b+a < len(suffix_maxs):
+            print(f"(1) cs+sm={curr_sum + suffix_maxs[b+a]}")
+            soln = max(soln, curr_sum + suffix_maxs[b+a])
         for i, n in enumerate(nums[b:], start=b):
             curr_sum -= nums[i-b]
             curr_sum += n
-            if i - a >= 0:
+            # Curr window is from (i-b:i]
+            # Look for the best for any prev window ending before or at i-b
+            if i - b >= 0:
+                print(f"cs+pm={curr_sum + prefix_maxs[i-b]}")
                 soln = max(soln, curr_sum + prefix_maxs[i-b])
+            # Look for the best for any prev window starting after i.
+            # This is equivalent to the prev window ending at i+a
             if i + a < len(suffix_maxs):
+                print(f"cs+sm{curr_sum + suffix_maxs[i+a]}")
                 soln = max(soln, curr_sum + suffix_maxs[i+a])
+            print(f"{i=} {i-b=} {n=} {curr_sum=} {soln=}")
         return soln
 
 
@@ -67,4 +84,13 @@ def test_3():
     a = 2
     b = 1
     expected = 1
+    assert Solution().solve(nums, a, b) == expected
+
+
+def test_4():
+    "WA"
+    nums = [2,1,0]
+    a = 1
+    b = 2
+    expected = 3
     assert Solution().solve(nums, a, b) == expected
