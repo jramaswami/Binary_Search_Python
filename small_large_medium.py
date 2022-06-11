8d193368bf333775b1fd63fda68a3487bc14865b
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-11 12:44:29 -0400

    WA

--- small_large_medium.py
+++ small_large_medium.py
@@ -4,15 +4,20 @@ jramaswami
 """
 
 
+import collections
+
+
 class Solution:
 
     def solve(self, nums):
-        T = []
+        T = collections.deque()
         for n in reversed(nums):
             if len(T) > 1 and n < T[0]:
                 return True
             if not T or n > T[-1]:
                 T.append(n)
+            while len(T) > 2:
+                T.popleft()
         return False
 
 
@@ -51,5 +56,12 @@ def test_6():
 def test_7():
     "WA"
     nums = [0,3,2,0]
+    expected = True
+    assert Solution().solve(nums) == expected
+
+
+def test_8():
+    "WA"
+    nums = [1,3,2,3]
     expected = True
     assert Solution().solve(nums) == expected
