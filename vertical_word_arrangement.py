ee6b1c4c1792b6e1bfa6d3405225df771dbb76a2
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-12 11:00:09 -0400

    WA

---
+++ vertical_word_arrangement.py
@@ -0,0 +1,26 @@
+"""
+binarysearch.com :: Vertical Word Arrangement
+jramaswami
+"""
+
+
+import itertools
+
+
+class Solution:
+
+    def solve(self, s):
+        return [''.join(t) for t in itertools.zip_longest(*s.split(), fillvalue=' ')]
+
+
+def test_1():
+    s = "abc def ghij"
+    expected = ["adg", "beh", "cfi", "  j"]
+    assert Solution().solve(s) == expected
+
+
+def test_2():
+    "WA"
+    s = "ab c"
+    expected = ["ac", "b"]
+    assert Solution().solve(s) == expected
