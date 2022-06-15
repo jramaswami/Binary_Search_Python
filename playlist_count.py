32007a17deeb4cd09102d040958b9a38b271693e
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-15 11:14:07 -0400

    Would be WA. I read the problem wrong. I missed that each song must be played once.

--- playlist_count.py
+++ playlist_count.py
@@ -4,36 +4,53 @@ jramaswami
 """
 
 
-import collections
-import functools
-
-
 class Solution:
 
     def solve(self, num_songs, playlist_size, cycle_length):
         MOD = pow(10, 9) + 7
+        # For the indexes i from cycle_length to playlist_size
+        # there are num_songs - cycle_length possible songs to choose.
+        # Example:
+        # 100 songs for a playlist of size 5 with a cycle of 2 is:
+        # 100 * 99 * 98 * 98 * 98
+        soln = 1
+        terms = []
+        for i in range(num_songs, num_songs - cycle_length, -1):
+            terms.append(i)
+            soln = (soln * i) % MOD
 
-        # dp[length][last cycle_length songs] = count
-        prev_dp = collections.defaultdict(int)
-        prev_dp[tuple()] = 1
-        next_dp = collections.defaultdict(int)
-        for p_size in range(1, playlist_size+1):
-            for prev_cycle in prev_dp:
-                next_cycle = list(prev_cycle[1:]) if len(prev_cycle) > cycle_length else list(prev_cycle)
-                for song in range(num_songs):
-                    if song not in next_cycle:
-                        next_cycle.append(song)
-                        key = tuple(next_cycle)
-                        next_dp[key] = (next_dp[key] + prev_dp[prev_cycle]) % MOD
-                        next_cycle.pop()
-            prev_dp, next_dp = next_dp, collections.defaultdict(int)
-
-        soln = 0
-        for t in prev_dp.values():
-            soln = (soln + t) % MOD
-        return soln
+        k = num_songs - cycle_length
+        t = playlist_size - cycle_length
+        terms.extend([k] * t)
+        soln = (soln * pow(k, t, MOD)) % MOD
+        print(len(terms), terms)
+        return soln % MOD
 
 
+
+def brute_force(num_songs, playlist_size, cycle_length):
+    MOD = pow(10, 9) + 7
+    visited = set()
+
+    def solve(acc):
+        if len(acc) == playlist_size:
+            if tuple(acc) in visited:
+                print('dupe', acc)
+            else:
+                print(acc)
+            visited.add(tuple(acc))
+            return 1
+
+        result = 0
+        for u in range(num_songs):
+            if u not in acc[-cycle_length:]:
+                acc.append(u)
+                result = (result + solve(acc)) % MOD
+                acc.pop()
+        return result
+
+    acc = []
+    return solve(acc)
 
 
 def test_1():
@@ -51,3 +68,28 @@ def test_2():
     expected = 317800737
     assert Solution().solve(num_songs, playlist_size, cycle_length) == expected
 
+
+def test_3():
+    num_songs = 100
+    playlist_size = 100
+    cycle_length = 50
+    expected = 437918130
+    assert Solution().solve(num_songs, playlist_size, cycle_length) == expected
+
+
+def test_4():
+    num_songs = 5
+    playlist_size = 5
+    cycle_length = 2
+    expected = 120
+    assert Solution().solve(num_songs, playlist_size, cycle_length) == expected
+
+
+
+if __name__ == '__main__':
+    num_songs = 5
+    playlist_size = 5
+    cycle_length = 2
+    expected = 120
+    soln = brute_force(num_songs, playlist_size, cycle_length)
+    print(soln, soln == expected)
