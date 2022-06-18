8e3776a6499813805de99d0eb7ca34c68ba8556c
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-18 14:44:32 -0400

    AC. Finally. With help.

--- kth_pair_distance.py
+++ kth_pair_distance.py
@@ -1,53 +1,41 @@
 """
 binarysearch.com :: Kth Pair Distance
 jramaswami
+
+https://www.youtube.com/watch?v=ym93rTBR4j8
+Had to convert zero-based to one-based.
 """
 
 
 class Solution:
 
     def solve(self, nums, k):
+        # Convert zero-based to one-based.
+        k += 1
+
         nums.sort()
 
-        lo = min(b - a for a, b in zip(nums[:-1], nums[1:]))
-        hi = nums[-1] - nums[0]
-        while lo <= hi:
-            # Guess a number, x.
-            x = lo + ((hi - lo) // 2)
-            # How many pair diffs are less than x.
-            lt = 0
-            # How many pair diffs are less than or equal to x.
+        def count_pairs_lte(guess):
+            "Return the number of pairs with distance <= guess."
             lte = 0
-            # What is the largest existing diff <= our guess, x.
-            y = 0
-
-            lt_i = 0
-            lte_i = 0
+            j = 1
             for i, _ in enumerate(nums):
-                while lt_i < len(nums) and nums[lt_i] - nums[i] < x:
-                    lt_i += 1
-                while lte_i < len(nums) and nums[lte_i] - nums[i] <= x:
-                    y = max(nums[lte_i] - nums[i], y)
-                    lte_i += 1
+                while j < len(nums) and nums[j] - nums[i] <= guess:
+                    j += 1
+                lte += (j - i - 1)
+            return lte
 
-                # le_i now points to the first j such that nums[i] + any nums[i+1:j] < x
-                # lte_i now points to the first j such that nums[i] + any nums[i+1:j] <= x
-                lt += lt_i - i - 1
-                lte += lte_i - i - 1
-
-            if lt <= k < lte:
-                # If lt <= k < lte then our guess, x, produces the right
-                # number of pairs sums to that x is the k-th pair sum.
-                # Return the largest existing difference <= x.
-                return y
-            elif k < lt:
-                # If k < lt, there are too many pairs sums that are less than
-                # k for our given guess.  We need a smaller guess to reduce the
-                # the number of pair sums less than x. Otherwise, increase the
-                # guess.
-                hi = x - 1
+        # Binary search for the answer.
+        lo = 0
+        hi = nums[-1] - nums[0]
+        while lo < hi:
+            guess = lo + ((hi - lo) // 2)
+            if count_pairs_lte(guess) >= k:
+                hi = guess
             else:
-                lo = x + 1
+                lo = guess + 1
+        return lo
+
 
 
 def test_1():
