46bea434828499b7d58ecbe57195251554a12fa3
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-18 12:37:54 -0400

    Still TLE.

--- kth_pair_distance.py
+++ kth_pair_distance.py
@@ -9,78 +9,45 @@ class Solution:
     def solve(self, nums, k):
         nums.sort()
 
-        def find_possible(smallest=True):
-            # print(f"find_possible({smallest=})")
-            lo = min(b - a for a, b in zip(nums[:-1], nums[1:]))
-            hi = nums[-1] - nums[0]
-            result = lo
-            if smallest:
-                result = hi
+        lo = min(b - a for a, b in zip(nums[:-1], nums[1:]))
+        hi = nums[-1] - nums[0]
+        while lo <= hi:
+            # Guess a number, x.
+            x = lo + ((hi - lo) // 2)
+            # How many pair diffs are less than x.
+            lt = 0
+            # How many pair diffs are less than or equal to x.
+            lte = 0
+            # What is the largest existing diff <= our guess, x.
+            y = 0
 
-            while lo <= hi:
-                # Guess a number, x.
-                x = lo + ((hi - lo) // 2)
-                # How many pair diffs are less than x.
-                lt = 0
-                # How many pair diffs are less than or equal to x.
-                lte = 0
+            lt_i = 0
+            lte_i = 0
+            for i, _ in enumerate(nums):
+                while lt_i < len(nums) and nums[lt_i] - nums[i] < x:
+                    lt_i += 1
+                while lte_i < len(nums) and nums[lte_i] - nums[i] <= x:
+                    y = max(nums[lte_i] - nums[i], y)
+                    lte_i += 1
 
-                lt_i = 0
-                lte_i = 0
-                for i, _ in enumerate(nums):
-                    # m - n < x
-                    # m < x + n
-                    while lt_i < len(nums) and nums[lt_i] - nums[i] < x:
-                        lt_i += 1
-                    while lte_i < len(nums) and  nums[lte_i] - nums[i] <= x:
-                        lte_i += 1
+                # le_i now points to the first j such that nums[i] + any nums[i+1:j] < x
+                # lte_i now points to the first j such that nums[i] + any nums[i+1:j] <= x
+                lt += lt_i - i - 1
+                lte += lte_i - i - 1
 
-                    # le_i now points to the first j such that nums[i] + any nums[i+1:j] < x
-                    # lte_i now points to the first j such that nums[i] + any nums[i+1:j] <= x
-                    lt += lt_i - i - 1
-                    lte += lte_i - i - 1
-
-                # TODO: can I figure out what x should be from the fact that
-                #       I have a guess and I kept track of the maximum difference
-                #       that is less than or equal to x.
-
+            if lt <= k < lte:
                 # If lt <= k < lte then our guess, x, produces the right
                 # number of pairs sums to that x is the k-th pair sum.
-                if lt <= k < lte:
-                    if smallest:
-                        hi = x - 1
-                        result = min(result, x)
-                    else:
-                        lo = x + 1
-                        result = max(result, x)
+                # Return the largest existing difference <= x.
+                return y
+            elif k < lt:
                 # If k < lt, there are too many pairs sums that are less than
                 # k for our given guess.  We need a smaller guess to reduce the
                 # the number of pair sums less than x. Otherwise, increase the
                 # guess.
-                elif k < lt:
-                    hi = x - 1
-                else:
-                    lo = x + 1
-            return result
-
-        def difference_exists(x):
-            lt_i = 0
-            for i, _ in enumerate(nums):
-                # m - n < x
-                # m < x + n
-                while lt_i < len(nums) and nums[lt_i] - nums[i] < x:
-                    lt_i += 1
-                # le_i now points to the first j such that nums[i] + any nums[i+1:j] < x
-                # lte_i now points to the first j such that nums[i] + any nums[i+1:j] <= x
-                if lt_i < len(nums) and nums[lt_i] - nums[i] == x:
-                    return True
-            return False
-
-        smallest_possible = find_possible(smallest=True)
-        largest_possible = find_possible(smallest=False)
-        for d in range(smallest_possible, largest_possible+1):
-            if difference_exists(d):
-                return d
+                hi = x - 1
+            else:
+                lo = x + 1
 
 
 def test_1():
