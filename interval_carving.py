2b97778535a48bc59f609671aa73fb63ff19d6b4
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-25 13:08:47 -0400

    WA

--- interval_carving.py
+++ interval_carving.py
@@ -10,6 +10,14 @@ class Solution:
         cut_intervals = []
         for interval in intervals:
             if interval[1] < cut[0]:
+                # Outside
+                # iiiii
+                #        xxxx
+                cut_intervals.append(interval)
+            elif cut[1] < interval[0]:
+                # Outside
+                #        iiiii
+                #  xxxx
                 cut_intervals.append(interval)
             elif cut[1] < interval[0]:
                 cut_intervals.append(interval)
@@ -18,14 +26,22 @@ class Solution:
                 #    iiii
                 # xxxxxxxxx
                 pass
-            else:
+            elif interval[0] <= cut[0] and cut[1] <= interval[1]:
+                # Overlaps
+                # iiiiiiiii
+                #    xxx
+                cut_intervals.append([interval[0], cut[0]])
+                cut_intervals.append([cut[1], interval[1]])
+            elif interval[0] <= cut[0] <= interval[1]:
                 # Intersects
-                # iiii          iiii
-                #   xxxxx     xxxx
-                if interval[0] <= cut[0] <= interval[1]:
-                    cut_intervals.append([interval[0], cut[0]])
-                else:
-                    cut_intervals.append([cut[1], interval[1]])
+                # iiii
+                #   xxxxx
+                cut_intervals.append([interval[0], cut[0]])
+            elif cut[0] <= interval[0] <= cut[1]:
+                # Intersects
+                #   iiii
+                # xxxx
+                cut_intervals.append([cut[1], interval[1]])
 
         return cut_intervals
 
@@ -39,7 +55,16 @@ def test_1():
 
 
 def test_2():
+    "WA"
     intervals = [[3, 8]]
     cut = [5, 5]
     expected = [[3,5], [5,8]]
+    assert Solution().solve(intervals, cut) == expected
+
+
+def test_3():
+    "WA"
+    intervals = [[5, 8]]
+    cut = [7, 8]
+    expected = [[5,7]]
     assert Solution().solve(intervals, cut) == expected
