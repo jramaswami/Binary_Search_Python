6e6dbb6f75cc88bb5e93e56be3e12c83a9ef25ba
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-10 16:46:43 -0400

    Before gutting code to use a treap.

--- ticket_order.py
+++ ticket_order.py
@@ -7,9 +7,71 @@ jramaswami
 import collections
 
 
+def treap_subtree_sum(treap):
+    if treap is None:
+        return 0
+    return treap.subtree_sum
+
+def treap_subtree_size(treap):
+    if treap is None:
+        return 0
+    return treap.subtree_size
+
+def treap_recalculate(treap):
+    if treap is not None:
+        treap._recalculate()
+
+def split_treap(treap, x):
+    if treap is None:
+        return [None, None]
+    if treap.children[0] and treap.data >= x:
+        left_result = split_treap(treap.children[0], x)
+        treap.children[0] = left_result[1]
+        treap._recalculate()
+        return [left_result[0], treap]
+    else:
+        right_result = split_treap(treap_children[1], x)
+        treap.children[1] = right_result
+        treap._recalculate()
+        return [treap, right_result]
+
+def merge_treap(left_treap, right_treap):
+    if left_treap is None:
+        return right_treap
+    if right_treap is None:
+        return left_treap
+    if left_treap.priority < right_treap.priority:
+        left_treap.children[1] = merge_treap(left_treap.children[1], right_treap)
+        treap_recalculate(left_treap)
+        return left_treap
+    else:
+        right_treap.children[0] = merge_treap(left_treap, right_treap.children[0])
+        treap_recalculate(right_treap)
+        return right_treap
+
+
+class Treap:
+    def __init__(self, data, priority):
+        self.data = data
+        # Could be random.
+        self.priority = priority
+        self.children = [None, None]
+        self._recalculate()
+
+    def _recalculate(self):
+        self.subtree_size = 1
+        self.subtree_sum = self.data
+        for child in self.children:
+            if child is not None:
+                self.subtree_size += child.subtree_size
+                self.subtree_sum += child.subtree_sum
+
 class Solution:
 
     def solve(self, tickets):
+        # TODO: Need a datastructure that allows you to get the sum of elements
+        #       less than k and a count of elements greater than or equal to k.
+        #       Some kind of balanced binary tree --> a Treap maybe?
         fwd_freqs = collections.defaultdict(int)
         soln = list(tickets)
         for i, t in enumerate(tickets):
