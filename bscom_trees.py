ff3d4f6f50ad2aee3e4a01a9f6d753b68c7920b6
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-21 12:10:31 -0400

    Utilities for testing binary tree problems.

--- bscom_trees.py
+++ bscom_trees.py
@@ -12,12 +12,18 @@ class Tree:
         self.right = right
 
     def __repr__(self):
-        return f"Tree(val={self.val}, left={self.left}, right={self.right}"
+        return f"Tree(val={self.val}, left={self.left}, right={self.right})"
+
+    def __eq__(self, other):
+        if other is None:
+            return self is None
+        return self.val == other.val and self.left == other.left and self.right == other.right
 
 
 def make_tree(array):
     if array == None:
         return None
+    if len(array) == 1:
+        return Tree(array[0], None, None)
     val, left, right = array[0], array[1], array[2]
     return Tree(val, make_tree(left), make_tree(right))
-
