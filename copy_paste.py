dc2683e3ed314135b95fe801dee5f46a3c75ae09
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-30 14:24:08 -0400

    AC

--- copy_paste.py
+++ copy_paste.py
@@ -4,24 +4,21 @@ jramaswami
 """
 
 
-import functools
-
-
 class Solution:
 
     def solve(self, n):
+        # dp[i] = max chars at this operation.
+        dp = [0 for _ in range(n+1)]
+        for i, _ in enumerate(dp[:-1]):
+            # At this point you can insert a single char and be done.
+            dp[i+1] = max(dp[i+1],dp[i] + 1)
+            # Or You can copy the buffer an paste as many times as possible.
+            clip = dp[i] * 2
+            for j, _ in enumerate(dp[i+2:], start=i+2):
+                dp[j] = max(dp[j], clip)
+                clip += dp[i]
+        return dp[-1]
 
-        @functools.cache
-        def cp(i, buff, clip):
-            if i >= n:
-                return buff
-            return max(
-                cp(i+1, buff, buff),        # Copy buffer to clipboard.
-                cp(i+1, buff+clip, clip),   # Paste clipboard to buffer.
-                cp(i+1, 1+buff, clip)       # Insert one into buffer.
-            )
-
-        return cp(0, 0, 0)
 
 def test_1():
     n = 6
