bd4f002a589191c177ea4abfbd56c21066795fc3
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-01 07:02:04 -0400

    AC

--- n_lexicographic_integers.py
+++ n_lexicographic_integers.py
@@ -38,6 +38,7 @@ class Solution:
     def solve(self, n):
         return [t.x for t in sorted(Digits(k) for k in range(1, n+1))]
 
+
 def test_1():
     n = 12
     expected = [1, 10, 11, 12, 2, 3, 4, 5, 6, 7, 8, 9]
