44fd548bd660432d10c0b80bb9559a086f0120a8
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-15 14:44:50 -0400

    TLE

--- anagram_difference.py
+++ anagram_difference.py
@@ -7,33 +7,35 @@ jramaswami
 import math
 
 
+import functools
+
+
 class Solution:
 
     def solve(self, s0, s1):
-        t0 = list(s0)
-        t1 = list(s1)
 
-        def solve0(i):
-            if t0 == t1:
+        @functools.cache
+        def solve0(i, t):
+            if t == s1:
                 return 0
 
             if i >= len(s0):
                 return math.inf
 
             # Make no swaps
-            result = solve0(i + 1)
+            result = solve0(i + 1, t)
 
             for j, _ in enumerate(s0):
+                t0 = list(t)
                 t0[i], t0[j] = t0[j], t0[i]
                 result = min(
                     result,
-                    1 + solve0(i + 1)
+                    1 + solve0(i + 1, "".join(t0))
                 )
-                t0[i], t0[j] = t0[j], t0[i]
 
             return result
 
-        return solve0(0)
+        return solve0(0, s0)
 
 
 def test_1():
@@ -48,4 +50,12 @@ def test_2():
     s0 = "gbmqzfa"
     s1 = "qbgzamf"
     expected = 5
+    assert Solution().solve(s0, s1) == expected
+
+
+def test_3():
+    "TLE"
+    s0 = "fxaaieki"
+    s1 = "aaeixkif"
+    expected = 6
     assert Solution().solve(s0, s1) == expected
