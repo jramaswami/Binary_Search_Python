cf7b62778feeb06871ce0f53afb63dbd9a280ccb
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-20 13:50:35 -0400

    AC

--- subsequence_widths.py
+++ subsequence_widths.py
@@ -12,24 +12,26 @@ class Solution:
         # Sort the numbers.
         nums.sort()
         print(nums)
-        for lf, mn in enumerate(nums):
-            for rt, mx in enumerate(nums[lf:], start=lf):
-                # Since the array is sorted, we know that nums[lf] is the
-                # minimum in nums[lf:rt).  We also no that nums[rt] is
-                # the maximum in nums[lf:rt).  So, how many subsequences
-                # can we form?  We must leave in nums[lf] and nums[rt] to
-                # maintain our invariants, but we can remove any of the
-                # (rt-lf = n) elements in the middle.  This will be the sum
-                # of nCk for k in the range [0:k], which is 2^n = s.  So
-                # there are s differnt subsequences with a width of (mn - mx).
-                n = max(0, rt - lf - 1)
-                w = mx - mn
-                s = pow(2, n, MOD)
-                t = (w * s) % MOD
-                print(f"{lf=} {mn=} {rt=} {mx=} {n=} {s=} {t=} {w=}")
-                soln += t
-                soln %= MOD
-        return soln % MOD
+        N = len(nums)
+        for i, n in enumerate(nums):
+            # n will be the maximum for all subsequences [0:i).
+            k = i
+            s = pow(2, k, MOD)
+            t = s * n
+            soln += t
+            soln %= MOD
+            # n will be the minimum for all subsequences [i:N).
+            k = N - i - 1
+            s = pow(2, k, MOD)
+            t = s * -n
+            if t < 0:
+                t += MOD
+            soln += t
+            soln %= MOD
+        return soln
+
+
+
 
 
 def test_1():
