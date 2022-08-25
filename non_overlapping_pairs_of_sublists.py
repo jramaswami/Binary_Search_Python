9a8224540da28950a4ad16993735b48e163708c6
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-25 14:04:46 -0400

    Changing direction, I think I am overcomplicating the solution.

--- non_overlapping_pairs_of_sublists.py
+++ non_overlapping_pairs_of_sublists.py
@@ -3,61 +3,96 @@ binarysearch.com :: Non-Overlapping Pairs of Sublists
 jramaswami
 """
 
-from collections import deque
+
+import collections
 
 
 MOD = pow(10, 9) + 7
 MOD_INV_2 = 500000004
+MOD_INV_24 = 41666667
 
 
-def compute_subarray_count(list_len):
-    return (list_len * (list_len + 1)) // 2
+def S(n):
+    # The number of possible sublists for a list of length n.
+    result = n
+    result *= (n + 1)
+    result %= MOD
+    result *= MOD_INV_2
+    result %= MOD
+    return result
+
+
+def T(n):
+    # Sum of i * S(n-i) for i from i to n - 1 if we add 3 to n.
+    n += 2
+    # Equivalent to n Choose 4.
+    result = n
+    result *= (n - 1)
+    result %= MOD
+    result *= (n - 2)
+    result %= MOD
+    result *= (n - 3)
+    result %= MOD
+    result *= MOD_INV_24
+    result %= MOD
+    return result
+
 
 class Solution:
 
-    # TODO: First count the number of splits in a contiguos array
-    #       Then count the number of pairs that go across arrays.
-
     def solve(self, nums, k):
-        soln = 0
+        # Determine the number of each length of sublist.
+        sublist_freqs = collections.Counter()
         curr = 0
-        subarray_count = 0
         for n in nums:
             if n >= k:
                 curr += 1
             else:
-                if curr > 0:
-                    # How many ways can you split a subarray of length L?
-                    soln = soln + (curr - 1)
-                    # How many subarrays can you make from a subarray of length L?
-                    M = compute_subarray_count(curr)
-
-                    # How many ways can you choose a subarray from the current
-                    # subarray along with a subarray from previous subarrays?
-                    soln = soln + (subarray_count * M)
-                    subarray_count += M
-                    curr = 0
-
-        if curr > 0:
-            # How many ways can you split a subarray of length L?
-            soln = soln + (curr - 1)
-            # How many subarrays can you make from a subarray of length L?
-            M = compute_subarray_count(curr)
-
-            # How many ways can you choose a subarray from the current
-            # subarray along with a subarray from previous subarrays?
-            soln = soln + (subarray_count * M)
-            subarray_count += M
-
-        print(f"{subarray_count=}")
+                sublist_freqs[curr] += 1
+        sublist_freqs[curr] += 1
+        del sublist_freqs[0]
+        print(f"{sublist_freqs=}")
+        # Count the total number of sublists.
+        total_sublists = 0
+        for l, f in sublist_freqs.items():
+            # A list of length l will have S(l) sublists.
+            t = S(l)
+            # There are f of those sublists.
+            t *= f
+            t %= MOD
+            total_sublists += t
+            total_sublists %= MOD
+        print(f"{total_sublists}")
+        soln = 0
+        for l, f in sublist_freqs.items():
+            # How many ways can we split a given sublist into
+            # nonoverlapping sublists.
+            x = T(l)
+            print(f"We can split a sublist of length {l} -> {x}")
+            # How many ways can we choose a sublist of our current
+            # sublist and pair it with a sublist of another sublist.
+            t = S(l)
+            s = t * (total_sublists - t)
+            s %= MOD
+            x += s
+            x %= MOD
+            # There are f of these lists.
+            x *= f
+            x %= MOD
+            soln += x
+            soln %= MOD
         return soln
-
-
-
 
 
 def test_1():
     nums = [3, 4, 4, 9]
     k = 4
-    expected = 5
+    expected = 57
+    assert Solution().solve(nums, k) == expected
+
+
+def test_2():
+    nums = [8, 12, 6, 15, 16, 1, 12, 20, 3, 1, 6, 11, 20, 10, 11, 13, 14, 8, 9, 10]
+    k = 10
+    expected = 262
     assert Solution().solve(nums, k) == expected
