3721473015adb19387e8cceda1aa5aafeade0a59
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-03 16:08:11 -0400

    RTE

--- seat_arrangement.py
+++ seat_arrangement.py
@@ -9,26 +9,60 @@ import functools
 
 class Solution:
     def solve(self, n, seats):
-
-        @functools.cache
-        def rec(i, x, p):
-            if x == 0:
-                return True
-
-            if i >= len(seats):
-                return False
-
-            if seats[i] == 1:
-                # This seat is occupied, we cannot sit here.
-                return rec(i+1, x, True)
-            elif p:
-                # Previous seat is occupied, we cannot sit here.
-                return rec(i+1, x, not p)
-            elif i + 1 == len(seats) or seats[i+1] == 0:
-                # Someone can sit here or not.
-                return rec(i+1, x-1, True) or rec(i+1, x, False)
+        # Greedy
+        seatslr = list(seats)
+        m = 0
+        for i, _ in enumerate(seatslr):
+            if i == 0:
+                if seatslr[i] == 0 and seatslr[i+1] == 0:
+                    seatslr[i] = 1
+                    m += 1
+            elif i + 1 >= len(seatslr):
+                if seatslr[i-1] == 0 and seatslr[i] == 0:
+                    seatslr[i] = 1
+                    m += 1
             else:
-                # Next seat is occupied, we cannot sit here.
-                return rec(i+1, x, False)
+                if seatslr[i-1] == 0 and seatslr[i] == 0 and seatslr[i+1] == 0:
+                    seatslr[i] = 1
+                    m += 1
+        if m == n:
+            return True
 
-        return rec(0, n, False)
+        m = 0
+        seatsrl = list(seats)
+        for i in range(len(seatsrl) - 1, -1, -1):
+            if i == 0:
+                if seatsrl[i] == 0 and seatsrl[i+1] == 0:
+                    seatsrl[i] = 1
+                    m += 1
+            elif i + 1 >= len(seatsrl):
+                if seatsrl[i-1] == 0 and seatsrl[i] == 0:
+                    seatsrl[i] = 1
+                    m += 1
+            else:
+                if seatsrl[i-1] == 0 and seatsrl[i] == 0 and seatsrl[i+1] == 0:
+                    seatsrl[i] = 1
+                    m += 1
+        return m == n
+
+
+def test_1():
+    n = 2
+    seats = [0, 0, 1, 0, 0, 0, 1]
+    expected = True
+    assert Solution().solve(n, seats) == expected
+
+
+def test_2():
+    n = 1
+    seats = [0, 1, 0]
+    expected = False
+    assert Solution().solve(n, seats) == expected
+
+
+def test_3():
+    "RTE"
+    n = 0
+    seats = [0]
+    expected = True
+    assert Solution().solve(n, seats) == expected
