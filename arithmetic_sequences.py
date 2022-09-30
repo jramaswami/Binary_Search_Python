c16421132a6f4eb213ea409991a2c92e582ca923
Joseph Ramaswami <jramaswami@gmail.com>
2022-09-30 17:55:35 -0400

    TLE

--- arithmetic_sequences.py
+++ arithmetic_sequences.py
@@ -10,50 +10,22 @@ import collections
 class Solution:
 
     def solve(self, nums):
-
-        # DP[end][delta] = list of lengths
         soln = 0
         dp = [collections.defaultdict(list) for _ in nums]
-        for i, left in enumerate(nums):
-            for j, right in enumerate(nums[i+1:], start=i+1):
-                delta = right - left
-                if delta in dp[i]:
-                    for t in dp[i][delta]:
-                        t0 = 1 + t
-                        dp[j][delta].append(t0)
-                        if t0 >= 3:
-                            soln += (t0 - 3 + 1)
-                else:
-                    # The first one.
-                    dp[j][delta].append(2)
+        for i, left in enumerate(nums[:-1]):
+            j, right = i + 1, nums[i+1]
+            delta = right - left
+            # Extend previous
+            if delta in dp[i]:
+                for t in dp[i][delta]:
+                    dp[j][delta].append(t + 1)
+                    if t + 1 >= 3:
+                        soln += 1
+            # Start from here.
+            dp[j][delta].append(2)
         return soln
 
 
-
 def test_1():
-    nums = [5, 11, 12, 7, 9, 13]
-    assert Solution().solve(nums) == 3
-
-
-def test_2():
-    nums = []
-    assert Solution().solve(nums) == 0
-
-
-def test_3():
-    nums = [1,2,3,4,5,6,7,8,9]
-    assert Solution().solve(nums) == 41
-
-
-def test_4():
-    nums = list(range(1000))
-    assert Solution().solve(nums) == 2781846
-
-
-def test_5():
-    """
-    WA: problem was that there was more than one way to arrive at a given
-    index with the given delta.
-    """
-    nums = [1,1,2,3]
-    assert Solution().solve(nums) == 2
+    nums =  [5, 7, 9, 11, 12, 13]
+    assert Solution().solve(nums) == 4
