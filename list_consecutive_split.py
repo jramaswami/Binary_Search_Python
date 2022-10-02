a7cf60e525c959f7175f91f22ca19d1772a8db61
Joseph Ramaswami <jramaswami@gmail.com>
2022-10-02 16:45:37 -0400

    WA

---
+++ list_consecutive_split.py
@@ -0,0 +1,29 @@
+"""
+binarysearch.com :: List Consecutive Split
+jramaswami
+"""
+
+
+import collections
+
+
+class Solution:
+
+    def solve(self, nums, k):
+        if len(nums) % k:
+            return False
+        freqs = collections.Counter(nums)
+        return max(freqs.values()) <= k
+
+
+def test_1():
+    nums = [3, 2, 3, 4, 5, 1]
+    k = 3
+    assert Solution().solve(nums, k) == True
+
+
+def test_2():
+    "WA"
+    nums = [0, 0]
+    k = 1
+    assert Solution().solve(nums, k) == True
